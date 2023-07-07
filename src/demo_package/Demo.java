2273b1252642e8400aa46317d7dbca6b540770d3
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-07-07 21:27:24 +0530

    Revision class

---
+++ src/demo_package/Demo.java
@@ -0,0 +1,16 @@
+package demo_package;
+
+public class Demo {
+
+ public static void main(String args[]) {
+
+  System.out.println("This is my first java code ");
+  System.out.println("kjfkjdsfajksjkajssfsfjfsa");
+
+ }
+
+ }
+
+
+
+
---
+++ src/demo_package/Demo1.java
@@ -0,0 +1,14 @@
+package demo_package;
+
+public class Demo1 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my second java code ");
+        System.out.println("This is second line ");
+
+
+    }
+
+}
