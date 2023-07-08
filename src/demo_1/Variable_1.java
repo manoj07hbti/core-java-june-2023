f9f791313cf65c7614b67f7161fb602ec48c1fe7
KeertiAbhay <keertikumari900@gmail.com>
2023-07-08 16:06:25 +0530

    add variable task

---
+++ src/demo_1/Variable_1.java
@@ -0,0 +1,21 @@
+package demo_1;
+
+public class Variable_1 {
+    public static void main(String []  args ){
+       String name = "Ram";
+       int age = 22;
+       double salary = 4500.00;
+       String dept = "IT";
+       int id = 108855;
+
+
+       System.out.println(name);
+       System.out.println(age);
+       System.out.println(salary);
+       System.out.println(dept);
+       System.out.println(id);
+
+
+    }
+
+}
