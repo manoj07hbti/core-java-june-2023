42f368580595ad3b9cf177c0f95539b5b7e05be8
RamD-1998 <maruti@1998>
2023-07-08 19:32:23 +0530

    Add some chanages

---
+++ src/Demo_java/Java1.java
@@ -0,0 +1,9 @@
+package Demo_java;
+
+public class Java1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is first java class");
+    }
+}
---
+++ src/Demo_java/Java_3.java
@@ -0,0 +1,12 @@
+package Demo_java;
+
+public class Java_3 {
+    public static void main(String[] args) {
+        System.out.println("this is third java class");
+        System.out.println("my name is ram dixit");
+        System.out.println("i am from Gwalior");
+        System.out.println("i am java developer");
+        System.out.println("i am backend developer");
+
+    }
+}
