c9f46a27fd048010aa47fc27ab7f32f1009fa53a
RamD-1998 <maruti@1998>
2023-07-08 21:01:47 +0530

    Add some chanages

---
+++ src/Java_5.java
@@ -0,0 +1,8 @@
+public class Java_5 {
+    public static void main(String[] args) {
+        System.out.println("this is first java class");
+        System.out.println("my name is ram dixit");
+        System.out.println("i live in gwalior");
+        System.out.println("my hobbie is playing bolleyball");
+    }
+}
