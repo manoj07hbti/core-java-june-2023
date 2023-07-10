d7d18ff11ee8748f37447a7de18454c9d19b8b75
RamD-1998 <maruti@1998>
2023-07-10 15:14:16 +0530

    hobbies

---
+++ src/Java_7.java
@@ -0,0 +1,9 @@
+public class Java_7 {
+    public static void main(String[] args) {
+    System.out.println("my name is ram dixit");
+    System.out.println("i am preparation mppsc");
+    System.out.println("freinds is my life");
+    System.out.println("my favorite car is fortuner)");
+    System.out.println("my dream bike is ktm ");
+    }
+}
