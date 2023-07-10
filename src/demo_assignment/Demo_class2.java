294ebe517fa3a477d5214043fcc2cb3535bd259f
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 19:20:15 +0530

    Demo introduction

--- src/demo_assignment/Demo_class2.java
+++ src/demo_assignment/Demo_class2.java
@@ -1,4 +1,11 @@
 package demo_assignment;
 
 public class Demo_class2 {
+    public static void main(String[] args) {
+        System.out.println("my name is akash");
+        System.out.println("my hobbies are reading books");
+        System.out.println(" i am currently study core java program ");
+        System.out.println("so i want to learn this deeply");
+        System.out.println("for development of my knowledge");
+    }
 }
