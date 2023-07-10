0cb79d6973c12b76b35405ba7dd260b56cf44cbb
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 19:27:38 +0530

    samsung models name

--- src/demo_assignment/Demo_class3.java
+++ src/demo_assignment/Demo_class3.java
@@ -1,4 +1,11 @@
 package demo_assignment;
 
 public class Demo_class3 {
+    public static void main(String[] args) {
+        System.out.println("samsung model name");
+        System.out.println("samsung s1");
+        System.out.println("samsung s2");
+        System.out.println("Samsung s11");
+        System.out.println("samsung s12");
+    }
 }
