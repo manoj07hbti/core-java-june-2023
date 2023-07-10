621c9dec745988ac92635c8f61ebbc3c0a4ca543
RamD-1998 <maruti@1998>
2023-07-10 15:27:00 +0530

    message

---
+++ src/Java_8.java
@@ -0,0 +1,9 @@
+public class Java_8 {
+    public static void main(String[] args) {
+    System.out.println("this is my first java class");
+    System.out.println("my name is ram dixit");
+    System.out.println("my dream is cleared mppsc exam");
+    System.out.println("my dream post SDM");
+    System.out.println("mppsc is third tier exam");
+    }
+}
