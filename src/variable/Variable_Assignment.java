fab23897e87c32b253fbe6b95a37df7456a1bef5
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-11 01:37:32 +0530

    Assignment String,int,double variable

--- src/variable/Variable_Assignment.java
+++ src/variable/Variable_Assignment.java
@@ -16,13 +16,52 @@ public class Variable_Assignment {
         System.out.println(" variable fruit name is " + fruitname);
 
         String boyname;
-        boyname="Rohan";
-        System.out.println("variable boyname is " + boyname );
+        boyname = "Rohan";
+        System.out.println("variable boyname is " + boyname);
 
-        String subjectname ;
-        subjectname="English";
+        String subjectname;
+        subjectname = "English";
         System.out.println("variable subjectname is " + subjectname);
 
+        int age;
+        age = 12;
+        System.out.println("age of rahul is " + age);
+
+        int number;
+        number = 99889;
+        System.out.println("Number variable is " + number);
+
+        long rupee;
+        rupee = 1000000000;
+        System.out.println(" variable of rupee is " + rupee);
+
+        int rollnumber;
+        rollnumber=04;
+        System.out.println("variable of Rollnumber is " + rollnumber );
+
+        long marks;
+        marks=89;
+        System.out.println("variable of marks is " + marks);
+
+        double salary;
+        salary=200000.250;
+        System.out.println("variable of salary is " + salary);
+
+        double price;
+        price=99.90;
+        System.out.println("variable of price is " +price);
+
+        double rate;
+        rate=53.26;
+        System.out.println("variable of rate is " +rate );
+
+        double mrp;
+        mrp=98.50;
+        System.out.println(" variable of mrp is " + mrp);
+
+        double oxygen;
+        oxygen=21;
+        System.out.println("percentage of oxygen on Earth is " + oxygen);
 
     }
 }
