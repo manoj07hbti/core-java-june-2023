1b29bc604bb11f51ba0f03dc08bd8d65c1d845f1
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 19:42:58 +0530

    Demo class5 java tools

--- src/demo_assignment/Demo_class5.java
+++ src/demo_assignment/Demo_class5.java
@@ -1,4 +1,12 @@
 package demo_assignment;
 
 public class Demo_class5 {
+    public static void main(String[] args) {
+        System.out.println("tools of java language");
+        System.out.println("java development kit");
+        System.out.println("java virtual machine");
+        System.out.println("java runtime environment");
+        System.out.println("java is platform independent");
+
+    }
 }
