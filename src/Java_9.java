45744e76e59a77ecb1332dd1f40cc906fcf0642d
RamD-1998 <maruti@1998>
2023-07-10 15:36:46 +0530

    food

---
+++ src/Java_9.java
@@ -0,0 +1,9 @@
+public class Java_9 {
+    public static void main(String[] args) {
+    System.out.println("this is my first java class");
+    System.out.println("my favorite dish is dosa");
+    System.out.println("my favorite food is south indian");
+    System.out.println("delicious food made up of south indian species");
+    System.out.println("south indian food is norma not oily");
+    }
+}
