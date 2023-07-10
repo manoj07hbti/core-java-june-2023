1e72d5c6dd94ad991e8702988e25c95a0ac242f9
pcz <bachchoomathur3@gmail.com>
2023-07-10 23:06:13 +0530

    Commit Variable Data..

---
+++ src/variable_data_type/Demo_Variable.java
@@ -0,0 +1,34 @@
+package variable_data_type;
+
+public class Demo_Variable{
+    public static void main(String[] args) {
+        String Country_Name="India";
+        System.out.println("Printing Country Name---> "  +Country_Name);
+        String country_Name="America";
+        System.out.println("Printing Second Country Name----> "  +country_Name);
+        String Cityzen="Indian Cityzen";
+        System.out.println("Printing Cityzen-----> "  +Cityzen);
+        String cityzen="American Cityzen";
+        System.out.println("Printing Ameriacan Cityzen---->" +cityzen);
+        String Passport="Indian Passport";
+        System.out.println("Printing Indian Passport----> "  +Passport);
+        int age=30;
+        System.out.println("Printing Age--->" +age);
+        int Marks=88;
+        System.out.println("Printing Marks--->"  +Marks);
+        int Pincode=283204;
+        System.out.println("Printing Pincode NUmber---->"  +Pincode);
+        int Mobile_RAM=8;
+        double salary=34000.76;
+        System.out.println("Printing salary---->"  +salary);
+        double Price=999.99;
+        System.out.println("Printing Price---->"  +Price);
+        double Employee_Salary=55000.89;
+        System.out.println("Ptinting Employee Salary---->"  +Employee_Salary);
+        Boolean Condition=true;
+        System.out.println("Printing Condition---->"  +Condition);
+        boolean Condition_Second=false;
+        System.out.println("Printing Condition Second--->"  +Condition_Second);
+
+    }
+}
