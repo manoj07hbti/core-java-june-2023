aa4052145b44ddde8c7e267ef97a70677fc8908f
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 20:30:02 +0530

    Demo class9 laptops name

--- src/demo_assignment/Demo_class9.java
+++ src/demo_assignment/Demo_class9.java
@@ -1,4 +1,11 @@
 package demo_assignment;
 
 public class Demo_class9 {
+    public static void main(String[] args) {
+        System.out.println("Dell");
+        System.out.println("Toshiba");
+        System.out.println("Thinkpad");
+        System.out.println("Lenovo");
+        System.out.println("MacBook");
+    }
 }
