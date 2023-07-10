553b0d9f4e79b823856b233ccefaa40769b2d750
Anoopk2021 <anoop.k.2017@gmail.com>
2023-07-10 22:14:10 +0530

    Demo Variable String Int Double

---
+++ src/variable/DemoVariableDay1.java
@@ -0,0 +1,79 @@
+package variable;
+
+public class DemoVariableDay1 {
+    public static void main(String[] args) {
+
+        //Data Type String
+        String countryName;
+        countryName="India";
+        System.out.println("My Country name is " +countryName);
+
+        String courseName;
+        courseName="Java";
+        System.out.println("My Course name is " +courseName);
+
+        String interviewername;
+        interviewername="Akansha";
+        System.out.println("Today's Interviewer Name " + interviewername);
+
+        String certificateName;
+        certificateName="MDCA";
+        System.out.println("My certificateName " + certificateName);
+
+        String placeName;
+        placeName="Hinjewadi";
+        System.out.println("My Place name is " +placeName);
+
+        // Data Type Int
+        int age;
+        age=30;
+        System.out.println("My Age is "+ age);
+
+        int days;
+        days=30;
+        System.out.println("Days in this month "+ days);
+
+        int month;
+        month=12;
+        System.out.println("Months in Year "+ month);
+
+        int time;
+        time=5;
+        System.out.println("Time is "+ time);
+
+        int pages;
+        pages=353;
+        System.out.println("Total Pages in my Book "+pages);
+
+        //Data Type Double
+        double price;
+        price=9888.55;
+        System.out.println("Price is "+price);
+
+        double salary;
+        salary=88000.75;
+        System.out.println("My Salary is " +salary);
+
+        double distance;
+        distance=45.23;
+        System.out.println("Distance from Pune to Mumbai is "+distance);
+
+        double number;
+        number=75.23;
+        System.out.println("My number in Physics " +number);
+
+        double cost;
+        cost=924.34;
+        System.out.println("My Pen cost is "+ 924.34);
+
+
+
+
+
+
+
+
+
+
+    }
+}
