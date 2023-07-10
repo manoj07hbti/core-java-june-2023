18c918ac9f59da94faf3c510b3f978dcbd2f7e7e
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 20:23:27 +0530

    Demo class7 states touch to up

--- src/demo_assignment/Demo_class8.java
+++ src/demo_assignment/Demo_class8.java
@@ -1,4 +1,14 @@
 package demo_assignment;
 
-public class Demo_class8 {
+public class Demo_class8{
+    public static void main(String[] args) {
+        System.out.println("States name near uttar padesh ");
+        System.out.println("Bihar");
+        System.out.println("uttarakhand");
+        System.out.println("Haryana");
+        System.out.println("Rajasthan");
+        System.out.println("Madhyapradesh");
+        System.out.println("Delhi");
+    }
+
 }
