1b1b448818199bb9f0e15583f12471b1dec6ca07
RamD-1998 <maruti@1998>
2023-07-10 15:04:25 +0530

    introduction

---
+++ src/Java_6.java
@@ -0,0 +1,10 @@
+public class Java_6 {
+    public static void main(String[] args) {
+        System.out.println("this is my first java class");
+        System.out.println("my name is ram dixit");
+        System.out.println("my favorite food is south india");
+        System.out.println("my dream destination is france");
+        System.out.println("my favorite coffe is americano");
+
+    }
+}
