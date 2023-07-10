9c174ba33af129040fc99249ad10bc6937909f04
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 19:34:05 +0530

    Demo class4 i phone models

--- src/demo_assignment/Demo_class4.java
+++ src/demo_assignment/Demo_class4.java
@@ -1,4 +1,12 @@
 package demo_assignment;
 
 public class Demo_class4 {
+    public static void main(String[] args) {
+        System.out.println("i phone models name");
+        System.out.println("i phone 11");
+        System.out.println("i phone 12");
+        System.out.println("i phone 13");
+        System.out.println("i phone 14");
+
+    }
 }
