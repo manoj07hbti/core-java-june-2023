5fb752033fd89a7b520340f28114c8decb75623c
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 20:36:14 +0530

    Demo class10 country name

--- src/demo_assignment/Demo_class10.java
+++ src/demo_assignment/Demo_class10.java
@@ -1,4 +1,12 @@
 package demo_assignment;
 
 public class Demo_class10 {
+    public static void main(String[] args) {
+        System.out.println("five country name");
+        System.out.println("India");
+        System.out.println("pakistan");
+        System.out.println("Bhutan");
+        System.out.println("China");
+        System.out.println("Nepal");
+    }
 }
