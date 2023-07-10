0b8ad35153ad3c1d256aa9890e9be824b9d06e2c
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 19:48:52 +0530

    Demo class6 oops concept

--- src/demo_assignment/Demo_class6.java
+++ src/demo_assignment/Demo_class6.java
@@ -1,4 +1,12 @@
 package demo_assignment;
 
 public class Demo_class6 {
+    public static void main(String[] args) {
+        System.out.println(" About oops 4 programs");
+        System.out.println("Encapsulation");
+        System.out.println("Abstraction");
+        System.out.println("Inheritance");
+        System.out.println("polymorphism");
+
+    }
 }
