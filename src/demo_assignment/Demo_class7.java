5cd8c0d33a8ca8e4d638c56e1eff39acaf883f4b
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-10 19:58:27 +0530

    Demo class7 About java

--- src/demo_assignment/Demo_class7.java
+++ src/demo_assignment/Demo_class7.java
@@ -1,4 +1,13 @@
 package demo_assignment;
 
+import java.sql.SQLOutput;
+
 public class Demo_class7 {
+    public static void main(String[] args) {
+        System.out.println("java is a language");
+        System.out.println("help in software development");
+        System.out.println("also help in making program");
+        System.out.println("java is platform independent language");
+        System.out.println("help in achievement software ");
+    }
 }
