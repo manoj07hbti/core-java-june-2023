2989bdab9fd57837677fbbddc45f8333ad89d2e3
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-11 01:08:37 +0530

    Assignment String variable

---
+++ src/variable/Variable_Assignment.java
@@ -0,0 +1,28 @@
+package variable;
+
+public class Variable_Assignment {
+    public static void main(String[] args) {
+
+        String cityname;
+        cityname = "Agra";
+        System.out.println("variable cityname is  " + cityname);
+
+        String Bookname;
+        Bookname = "Economics";
+        System.out.println("variable of bookname is " + Bookname);
+
+        String fruitname;
+        fruitname = " Mango";
+        System.out.println(" variable fruit name is " + fruitname);
+
+        String boyname;
+        boyname="Rohan";
+        System.out.println("variable boyname is " + boyname );
+
+        String subjectname ;
+        subjectname="English";
+        System.out.println("variable subjectname is " + subjectname);
+
+
+    }
+}
