cdae1a0bbd465ee70ad7e78b89ed1f79b3ed77ef
Uday6667 <yadavuday63@gmail.com>
2023-07-11 17:58:46 +0530

    I am completed my assingment variable

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -2,14 +2,67 @@ package demo;
 
 public class Demo {
 
-  public static void main(String []args) {
+    public static void main(String[] args) {
 
-      System.out.println("This is my first java program ");
+        {
+            // String data, we have learn string datatype
+            String cityName = "Junpur";
+            String AdressName = "sethapur";
+            String CourseName = "Full stack";
+            String JoblocationName = "pune";
+            String companyName = "Amazon";
+            String myName = "day";
+            System.out.println("printing cityName" + cityName);
+            System.out.println("printing AdressName" + AdressName);
+            System.out.println("printing CourseName" + CourseName);
+            System.out.println("printing JoblocationName" + JoblocationName);
+            System.out.println("printing companyName" + companyName);
+            System.out.println("printing myName" + myName);
 
-      System.out.println("Hello");
+            // int datatype: we have larning int datatype
 
-      System.out.println("World");
-  }
+            int age;
+            age=24;
+            int pin;
+            pin=222132;
+            int mybookprice;
+            mybookprice=200;
+            int myhouseNo;
+            myhouseNo=45;
+            int mycitycode;
+            mycitycode=05452;
 
+            System.out.println("age is " + age);
+            System.out.println("my pin is " + pin);
+            System.out.println("mybookpric is " + mybookprice);
+            System.out.println("myhouseNo " + myhouseNo);
+            System.out.println("mycitycode is " + mycitycode);
+
+//double datatype
+
+            double sugarprice;
+            sugarprice=45.50;
+            double tomatoprice;
+            tomatoprice=110.45;
+            double appleprice;
+            appleprice=250.80;
+            double garligprice;
+            garligprice=110.50;
+            double amolmilkprice;
+            amolmilkprice=40.80;
+            System.out.println("sugarprice is"+ sugarprice);
+            System.out.println("tomatoprice is" + tomatoprice);
+            System.out.println("applepric is"+ appleprice);
+            System.out.println("garligprice is"+ garligprice);
+            System.out.println("amolmilkprice is"+ amolmilkprice);
+
+
+
+
+
+
+        }
+    }
 }
+
 
