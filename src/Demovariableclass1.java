af5390f2530ed2a95adc5c426a79e58032c10fe1
RamD-1998 <dixitram633@gmail.com>
2023-07-11 22:45:15 +0530

    vaeriabledemo1

---
+++ src/Demovariableclass1.java
@@ -0,0 +1,24 @@
+public class Demovariableclass1 {
+    public static void main(String[] args) {
+     String name="Ram Dixit";
+     System.out.println("my name is:-"+name);
+
+        int age=22;
+        System.out.println("This is my age :-"+age);
+
+        double price=50;
+
+        System.out.println("This is my price :-"+price);
+
+        boolean isRaining = true;
+        boolean isSunny = false;
+
+        if (isRaining) {
+            System.out.println("It is raining. Take an umbrella!");
+        } else if (isSunny) {
+            System.out.println("It is sunny. Enjoy the day!");
+        } else {
+            System.out.println("The weather is neither raining nor sunny.");
+        }
+    }
+}
--- src/Java_10.java
+++
@@ -1,9 +0,0 @@
-public class Java_10 {
-    public static void main(String[] args) {
-    System.out.println("this is my first java class");
-    System.out.println("life is a journey but not normal");
-    System.out.println("life is a big challenge");
-    System.out.println("first chance is a last chance");
-    System.out.println("first attemp is a last attempt");
-    }
-}
