9c7575d56605a15c3d17e1f8df7e44cbf5604c7b
RamD-1998 <maruti@1998>
2023-07-12 17:43:27 +0530

    demovariableclass5

---
+++ src/variable/Demovariable5.java
@@ -0,0 +1,26 @@
+package variable;
+
+public class Demovariable5 {
+    public static void main(String[] args) {
+     String name="aman";
+     System.out.println("my name is:-"+name);
+
+        int age=22;
+        System.out.println("This is my age :-"+age);
+
+        double price=80;
+
+        System.out.println("This is my price :-"+price);
+
+        boolean isRaining = true;
+        boolean isSunny = false;
+
+        if (isRaining) {
+            System.out.println("It is raining. Take an umbrella!");
+        } else if (isSunny) {
+            System.out.println("It is sunny. Enjoy the day!");
+        } else {
+            System.out.println("The weather is neither raining nor sunny.");
+}
+    }
+}
