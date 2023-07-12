a8d5ed10948ea2992c4bd801cf7fc78bde8c8575
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 19:49:37 +0530

    my eight class

---
+++ src/practicep/Example_8.java
@@ -0,0 +1,16 @@
+package practicep;
+
+public class Example_8 {public static void main (String[]args){
+   System.out.println("do");
+    System.out.println("human");
+    System.out.println("nature");
+    System.out.println("Can i");
+    System.out.println("Shine");
+    System.out.println("Rise");
+    System.out.println("rock");
+    System.out.println("Damn");
+    System.out.println("Down");
+    System.out.println("Doll");
+
+}
+}
