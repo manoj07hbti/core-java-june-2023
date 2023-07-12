a2251a00f1c4309bd81de838f653f2f23e520bb0
Rohan030723 <rohan301997@gmail.com>
2023-07-13 02:54:51 +0530

    Create Package & Class

---
+++ src/demo_package/DemoAss00.java
@@ -0,0 +1,13 @@
+package demo_package;
+
+public class DemoAss00 {
+
+    public static void main (String[]args){
+
+        System.out.println("Assignment how to create package in java for further step check next demo01");
+
+    }
+
+
+
+}
