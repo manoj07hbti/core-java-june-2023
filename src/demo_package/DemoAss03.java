a53b2b7b8803ce75737e57fefd524cfbbe1b1712
Rohan030723 <rohan301997@gmail.com>
2023-07-13 03:00:47 +0530

    Name of Package should be start with small letter

---
+++ src/demo_package/DemoAss03.java
@@ -0,0 +1,8 @@
+package demo_package;
+
+public class DemoAss03 {
+    public static void main(String []args){
+        System.out.println(" For creating a package the starting name should be start with small letter eg - demo_package next follow Demo04 ");
+
+    }
+}
