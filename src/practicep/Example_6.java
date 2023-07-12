8d1894391f4404d9ec3819fa892738c173a0a20f
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 19:36:58 +0530

    my six class

---
+++ src/practicep/Example_6.java
@@ -0,0 +1,17 @@
+package practicep;
+
+public class Example_6 {
+    public static void main(String[]args){
+        System.out.println("Reason");
+        System.out.println("Hello Hr Sir");
+        System.out.println("My State");
+        System.out.println("Answer");
+        System.out.println("time");
+        System.out.println("Year");
+        System.out.println("Kind");
+        System.out.println("did");
+        System.out.println("Terminal");
+        System.out.println("run code");
+
+    }
+}
