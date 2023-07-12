590ec56779b9fb78e33644092102a9de9c15fe3e
Rohan030723 <rohan301997@gmail.com>
2023-07-13 03:07:22 +0530

    SRC file to create Package

---
+++ src/demo_package/DemoAss06.java
@@ -0,0 +1,8 @@
+package demo_package;
+
+public class DemoAss06 {
+     public static void main(String[] args) {
+         System.out.println("After entering in core java folder 2023 you will see src file follow next demo07  ");
+
+    }
+}
