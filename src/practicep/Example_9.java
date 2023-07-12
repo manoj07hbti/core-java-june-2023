0ec729807cf6a139d9d6da41fe5504c7c75b4349
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 20:01:40 +0530

    my nine class

---
+++ src/practicep/Example_9.java
@@ -0,0 +1,18 @@
+package practicep;
+
+public class Example_9 {
+    public static void main(String []args){
+        System.out.println("try");
+        System.out.println("all");
+        System.out.println("How Much");
+        System.out.println("How Many");
+        System.out.println("ten");
+        System.out.println("ok");
+        System.out.println("Name");
+        System.out.println("merge");
+        System.out.println("make");
+        System.out.println("many");
+
+
+    }
+}
