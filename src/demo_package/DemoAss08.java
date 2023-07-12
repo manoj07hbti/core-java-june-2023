f8a61b0235c16d1bf90bcd0bd027ee6bce10f01b
Rohan030723 <rohan301997@gmail.com>
2023-07-13 03:10:26 +0530

    Follow steps and Name the Class

---
+++ src/demo_package/DemoAss08.java
@@ -0,0 +1,11 @@
+package demo_package;
+
+public class DemoAss08 {
+public static void main(String []args){
+    System.out.println("Now to creat class under the file package so");
+    System.out.println("click on the package you create in that");
+    System.out.println("right click you will see java class");
+    System.out.println("Create a class as per the rule eg -DemoAss");
+    // Naming for class should be start with capital letter
+}
+}
