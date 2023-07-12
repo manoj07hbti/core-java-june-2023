74ef9f8481b364c948363f8273b1afd55ef1445a
Rohan030723 <rohan301997@gmail.com>
2023-07-13 03:13:29 +0530

    Final Step to perform code and print

---
+++ src/demo_package/DemoAss09.java
@@ -0,0 +1,17 @@
+package demo_package;
+
+public class DemoAss09 {
+    //So now as you know by following the steps you create package and class
+    // write a code in the Demo class there few command to follow to write and print
+    //1- public static void main(String arg[])
+    //2-  System.out.println(" ")
+    // the above this 2 simple steps are important to know
+    // eg see the following steps to print the statement
+    public static void main(String[]args){
+        System.out.println("Finally after creating a package and class in usr file ");
+        System.out.println("I can write a code and print it with the help of command void main string and system.out.print");
+        System.out.println("As now Finally the assignment is Done");
+        System.out.println("Now you can see the output of the code on the terminal");
+    }
+
+}
