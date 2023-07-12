064479c871f072e682451fa9fe2a5bc6011f36c4
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 19:30:26 +0530

    my forth class

---
+++ src/practicep/Example_4.java
@@ -0,0 +1,17 @@
+package practicep;
+
+public class Example_4 {
+    public static void main (String[]Args){
+
+        System.out.println("My Job");
+        System.out.println("My Example");
+        System.out.println("Hello Mam");
+        System.out.println("My Home");
+        System.out.println("Made by me");
+        System.out.println("Study");
+        System.out.println("hyphen");
+        System.out.println("Yes Sir");
+        System.out.println("Please Give Command");
+        System.out.println("Hello Sir");
+    }
+}
