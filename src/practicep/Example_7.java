8ad80b9365512b01706ab863facf518985d4e485
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 19:42:24 +0530

    my seven class

---
+++ src/practicep/Example_7.java
@@ -0,0 +1,17 @@
+package practicep;
+
+public class Example_7 {
+    public static void main(String[]args){
+        System.out.println("yes buddy");
+        System.out.println("Service");
+        System.out.println("Wifi");
+        System.out.println("Network");
+        System.out.println("Conduct");
+        System.out.println("Demo");
+        System.out.println("New");
+        System.out.println("kindly");
+        System.out.println("Tottal");
+        System.out.println("How");
+
+    }
+}
