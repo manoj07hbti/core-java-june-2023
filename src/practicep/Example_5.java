b06b4c460d73fe8204489e96a89da2518fe7f4c6
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 19:29:01 +0530

    my commit

---
+++ src/practicep/Example_5.java
@@ -0,0 +1,16 @@
+package practicep;
+
+public class Example_5 {
+    public static void main (String[]args){
+        System.out.println("Under coding");
+        System.out.println("Gatting");
+        System.out.println("good");
+        System.out.println("note");
+        System.out.println("Work");
+        System.out.println("Kind");
+        System.out.println("Done");
+        System.out.println("give command");
+        System.out.println("github");
+        System.out.println("copy");
+    }
+}
