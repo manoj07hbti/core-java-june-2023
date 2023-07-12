795aad366414784cf152c88f1540df8e0d76c33c
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 16:30:24 +0530

    my second class

--- src/practicep/Example_2.java
+++ src/practicep/Example_2.java
@@ -1,4 +1,16 @@
 package practicep;
 
-public class Example_2 {
+public class Example_2 {public static void main (String[]args){
+    System.out.println("Workspace");
+    System.out.println("Workhard");{
+    System.out.println("yes");}
+    System.out.println("My sallary");
+    System.out.println("My Joining ");
+    System.out.println("My Joining Date");
+    System.out.println("My country");
+    System.out.println("Hello World");
+    System.out.println("My Phone");
+    System.out.println("My Target");
+}
+
 }
