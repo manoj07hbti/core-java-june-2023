f24660e255facc6e43266c718c9daa473862e0ed
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 15:41:12 +0530

    Add the Change

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");}
-            System.out.println("Hello");}
-
-
-System.out.println("Hello this is demo comit");}{
-
+        System.out.println("Hello world!");
+            System.out.println("Hello");
+    }
 }
---
+++ src/practicep/Example_1.java
@@ -0,0 +1,8 @@
+package practicep;
+
+public class Example_1 {
+    public static void main(String[] args) {
+        System.out.println("My name is Ambika");
+    }
+
+}
