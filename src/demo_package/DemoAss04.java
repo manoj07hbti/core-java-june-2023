7144bed0b16778efbdd402c7dde7bb7e10f8226c
Rohan030723 <rohan301997@gmail.com>
2023-07-13 03:04:21 +0530

    Name of Class should be start with Capital letter

---
+++ src/demo_package/DemoAss04.java
@@ -0,0 +1,7 @@
+package demo_package;
+
+public class DemoAss04 {
+    public static void main(String []args){
+ System.out.println("To create a class under package in this the naming of class is done with starting with capital letter eg-DemoAss follow Demo05");
+    }
+}
