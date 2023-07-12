b7f0456eed091ecc78f266ca2f1b92d49879819b
Rohan030723 <rohan301997@gmail.com>
2023-07-13 03:05:47 +0530

    Now the time to create Package and Class

---
+++ src/demo_package/DemoAss05.java
@@ -0,0 +1,7 @@
+package demo_package;
+
+public class DemoAss05 {
+    public static void main(String []args){
+     System.out.println("As you how to name the package and class now the time how to create package and class follow Demo06");
+    }
+}
