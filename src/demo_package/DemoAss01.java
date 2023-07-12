28e542257e393fa8169bb499831ce654463d9d15
Rohan030723 <rohan301997@gmail.com>
2023-07-13 02:58:51 +0530

    Follow the next Demo02

---
+++ src/demo_package/DemoAss01.java
@@ -0,0 +1,10 @@
+package demo_package;
+
+public class DemoAss01 {
+
+
+    public static void main(String args[]) {
+
+        System.out.println("Demo01 - 1st of all main things to create package and class is follow next demo02");
+    }
+}
