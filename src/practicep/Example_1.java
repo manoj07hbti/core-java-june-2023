6cb120ea10ea13ca94841034e615602f9fa8b72d
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 16:04:58 +0530

    my practice

--- src/practicep/Example_1.java
+++ src/practicep/Example_1.java
@@ -3,6 +3,15 @@ package practicep;
 public class Example_1 {
     public static void main(String[] args) {
         System.out.println("My name is Ambika");
+        System.out.println("My District");
+        System.out.println("My Laptop");
+        System.out.println("Sirsaganj");
+        System.out.println("Firozabad");
+        System.out.println("My Package");
+        System.out.println("Hello World");
+        System.out.println("Hello class");
+        System.out.println("My Game");
+        System.out.println("My company");
     }
 
 }
