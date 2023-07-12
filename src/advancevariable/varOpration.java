d69fcb3f339ce01084795a36cd2cad830754f039
Ankita <ankitachougule19@gmail.com>
2023-07-12 23:26:21 +0530

    variableassignment

---
+++ src/advancevariable/varOpration.java
@@ -0,0 +1,19 @@
+package advancevariable;
+
+public class varOpration {
+    public static void main(String[] args) {
+        int a=2;
+        int square=a*a;
+        System.out.println("square of number is :"+square);
+
+        int cube=a*a*a;
+        System.out.println("Cube of number is:"+cube);
+
+        int sal=35000;
+        double profit=(double) (sal*10/100);
+
+
+        System.out.println("Total profit:"+profit);
+
+    }
+}
---
+++ src/variableassign/VariableFirstDemo.java
@@ -0,0 +1,66 @@
+package variableassign;
+
+public class VariableFirstDemo {
+
+    public static void main(String[] args) {
+
+        //syntax:datatype variablename
+
+        int flat_no;
+        flat_no=503;
+        System.out.println(flat_no);
+
+
+
+        int regiNum;
+        regiNum=12534;
+        System.out.println(regiNum);
+
+        int shoeNo;
+        shoeNo=8;
+        System.out.println(shoeNo);
+
+        int  bikeNo;
+        bikeNo=7217;
+        System.out.println(bikeNo);
+
+
+        String education;
+        education="computer engineer";
+        System.out.println(education);
+
+
+        String add;
+        add="Laxmi deep society";
+        System.out.println(add);
+
+        String fname;
+        fname="Prakash";
+        System.out.println(fname);
+
+        String nativePlace;
+        nativePlace="Kolhapur";
+        System.out.println(nativePlace);
+
+        String name;
+        name="Ankita";
+        System.out.println(name);
+
+        double sal;
+        sal=25000.52;
+        System.out.println(sal);
+
+        double area;
+        area=254.52;
+        System.out.println(area);
+
+
+
+    }
+
+
+
+}
+
+
+
