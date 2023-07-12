6fde06e6382c94f4e4aba49ccba649b56395fcf0
RamD-1998 <maruti@1998>
2023-07-12 10:54:50 +0530

    vaeriabledemo2

---
+++ src/variable/Demovariableclass2.java
@@ -0,0 +1,22 @@
+package variable;
+
+public class Demovariableclass2 {
+    public static void main(String[] args) {
+    int age=23;
+    System.out.println("this is my age:-"+age);
+
+    double price=60;
+    System.out.println("this is my price:-"+price);
+
+        boolean isRaining = true;
+        boolean isSunny = false;
+
+        if (isRaining) {
+            System.out.println("It is raining. Take an umbrella!");
+        } else if (isSunny) {
+            System.out.println("It is sunny. Enjoy the day!");
+        } else {
+            System.out.println("The weather is neither raining nor sunny.");
+        }
+    }
+}
