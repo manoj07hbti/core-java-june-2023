a0fcd52ccce5d980ee84b2a81979669b5a36277d
Rohan030723 <rohan301997@gmail.com>
2023-07-13 02:57:43 +0530

    Naming nature Package & Class

---
+++ src/demo_package/DemoAss02.java
@@ -0,0 +1,11 @@
+package demo_package;
+
+public class DemoAss02 {
+
+    public static void main(String []args ) {
+
+        System.out.println("To create package and class the naming nature is different follow Demo03");
+
+    }
+}
+
