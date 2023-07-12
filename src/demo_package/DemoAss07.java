6211cd5db7bc4b2b60ea90b704d759253d1eafef
Rohan030723 <rohan301997@gmail.com>
2023-07-13 03:09:06 +0530

    Follow steps and Name the Package

---
+++ src/demo_package/DemoAss07.java
@@ -0,0 +1,11 @@
+package demo_package;
+
+public class DemoAss07 {
+    public static void main(String []args)
+    {
+     System.out.println("As you right click on SRC file you will ");
+        System.out.println("see new in that down side you will see package");
+        System.out.println("click on it and create package as per the rule eg-demopackage ");
+        //Naming of the package is to start with small letter only
+    }
+}
