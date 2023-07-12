b83e3f11f4c9e0daa012339ecdf6a1c694185515
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 20:09:58 +0530

    my ten class

---
+++ src/practicep/Example_10.java
@@ -0,0 +1,20 @@
+package practicep;
+
+public class Example_10 {
+    public static void main(String[]args){
+        System.out.println("tool");
+
+        System.out.println("take");{
+
+        }
+        System.out.println("town");
+
+        System.out.println("under");
+        System.out.println("my rule");
+        System.out.println("My clock");
+        System.out.println("main class");
+        System.out.println("happy");
+        System.out.println("hide");
+        System.out.println("Humble");
+        }
+    }
