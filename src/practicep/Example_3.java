9b0eecea19bc4600ec164bec58cce0abd4bc8305
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-12 19:05:14 +0530

    my Third class

---
+++ src/practicep/Example_3.java
@@ -0,0 +1,17 @@
+package practicep;
+
+public class Example_3 {
+    public static void main (String[]args){
+
+        System.out.println("Hello Void main");
+        System.out.println("Hello My Hope");
+        System.out.println("Double");
+        System.out.println("How Are You");
+        System.out.println("All Correct");
+        System.out.println("Hello Process");
+        System.out.println("Coding");
+        System.out.println("Finished");
+        System.out.println("Digital");
+        System.out.println("Main Class");
+    }
+}
