66e7c8070ada20a6e3963d6537a4013481f33b11
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-13 22:57:12 +0530

    my veriable 2

---
+++ src/practicep/ExampleVeriable.java
@@ -0,0 +1,52 @@
+package practicep;
+
+public class ExampleVeriable {
+
+    public static void main(String[] args) {
+        String name= "neha";
+        String cityname= "Firozabad";
+        String Address = "jain nagar ";
+        String landmark= "shiv temple";
+        String Road = "agra";
+        System.out.println("My name is neha:+name");
+        System.out.println("My city name is Firozabad:"+name);
+        System.out.println("my Address is jain nagar:"+Address);
+        System.out.println("my landmark is shiv temple:"+landmark);
+        System.out.println("my road name is agra:"+Road);
+
+
+        int age=24;
+        int height=5;
+        int weight=48;
+        int code=283203;
+        int road=7;
+        System.out.println("my age is 24:"+name);
+        System.out.println("my height is 5: "+height);
+        System.out.println("my weight is 48:"+weight);
+        System.out.println("My code is 283203:"+code);
+        System.out.println("my road is 7:"+road);
+
+
+        Double height2= 5.3;
+        double sallary=00.00;
+        double percentage=89.9;
+        double mobileweight=9386;
+        double time=22.12;
+        System.out.println("My Double height2 is :"+height2);
+        System.out.println("my sallary is 00.00:"+sallary);
+        System.out.println("my percantage is 89.9:"+percentage);
+        System.out.println("my mobile weight is 9386:"+mobileweight);
+        System.out.println("my time is 22.12:"+time);
+
+
+     Boolean flag;
+        flag=false;
+        System.out.println("flag is:"+flag);
+
+
+    }
+
+
+
+
+}
