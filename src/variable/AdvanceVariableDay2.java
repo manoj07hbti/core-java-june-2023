c0c1582be9e9b75a762a93270b479a3c8f556b1a
Mayank6918-svh <mayanksingh25104@gmail.com>
2023-07-14 12:18:37 +0530

    addition,subtraction,multiplication

---
+++ src/variable/AdvanceVariableDay2.java
@@ -0,0 +1,47 @@
+package variable;
+
+public class AdvanceVariableDay2 {
+    public static void main(String[] args) {
+
+
+int a=10;
+int b=9;
+int sum=a+b;
+System.out.println("Addition of a and b is"+sum);
+
+int c=10;
+int d=8;
+int cd=c+d;
+System.out.println("Addition of c and d is"+cd);
+
+int e=1000;
+int f=999;
+int ef=e+f;
+System.out.println("Addition of e and f is"+ef);
+
+int a1=100;
+int b1=99;
+int sub=a1-b1;
+System.out.println("Subtraction of a1 and b1 is"+sub);
+
+int c1=89;
+int d1=98;
+int c1d1=c1-d1;
+System.out.println("Subtraction of c1 and d1 is "+c1d1);
+
+int ab=22;
+int ba=0;
+int multiply=ab*ba;
+System.out.println("Multiplication of ab and ba "+multiply);
+
+int ac=1;
+int bc=0;
+int acbc=ac*bc;
+System.out.println("Multiplication of ac and bc "+acbc);
+
+
+
+
+
+    }
+}
