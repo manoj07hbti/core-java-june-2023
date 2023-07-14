ad9116d9889260be52fcfe4f74bbf259184a3d72
pcz <bachchoomathur3@gmail.com>
2023-07-14 11:29:36 +0530

    Commit advance Variable Data Type

---
+++ src/variable_data_type/Assignment_Variable_Type.java
@@ -0,0 +1,51 @@
+package variable_data_type;
+
+public class Assignment_Variable_Type {
+    public static void main(String []args){
+        int a=10;
+        int b=5;
+         a+=b; // addition
+
+        System.out.println("Printing Addition a--->"  +a);
+
+        a-=b; // Subtraction
+
+        System.out.println("Printing Subtraction a--->"  +b);
+        a*=b; // multiplication
+
+        System.out.println("Printing Multiplication a--=>"   +a);
+        a/=b; // Division
+
+        System.out.println("Printing Division a--->" +a);
+
+        int i=5;
+
+        i++; // increment variable;
+
+        System.out.println("Printing Increment Variable ----> " +i);
+
+        i--; // decrement variable;
+
+        System.out.println("Printing decrement variable----> " +i);
+
+        int x=3;
+
+       int y=x*x; // squire
+
+        System.out.println("Printing Square Variable--->"  +y);
+
+        y=x*x*x;  // que
+
+        System.out.println("Printing Que-->" +y);
+
+        int t=6;
+        int m=7;
+        double div=(double) t/m;
+        System.out.println("Printing Division---->"  +div);
+
+
+
+
+
+    }
+}
