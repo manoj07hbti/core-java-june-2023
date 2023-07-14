8324eaf2b0a69bb16e38045f434fe4b83a6cde04
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-15 04:12:14 +0530

    Advance percentage variable

---
+++ src/variable/Percentage.java
@@ -0,0 +1,30 @@
+package variable;
+
+public class Percentage {
+    public static void main(String[] args) {
+        double number=1000;
+        double percentage =1000*10/100;
+        System.out.println("printing the value of 1000 as 10 percentage is : " + percentage);
+
+        number=100;
+        percentage=100*5/100;
+        System.out.println("printing the value of 100 as 10 percentage is : " + percentage);
+
+        number=10000;
+        percentage=10000*5/100;
+        System.out.println("printing value of 10000 as 5 percentage is : " + percentage);
+
+        number=80;
+        percentage=80*2/100;
+        System.out.println("printing the value of 80 as 2 percentage is : " + percentage);
+
+        number=45000;
+        percentage=45000*2/100;
+        System.out.println("printing the value of 45000 as 2 percentage is : " + percentage);
+
+        number=30000;
+        percentage=30000*5/100;
+        System.out.println("printing the value of 30000 as 5 percentage is : " + percentage);
+    }
+
+}
