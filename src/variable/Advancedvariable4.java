566312f1b76511026032a4882a7a77965e44efc9
RamD-1998 <maruti@1998>
2023-07-14 20:31:33 +0530

    advanced2square,cube,percentage,programme3

---
+++ src/variable/Advancedvariable4.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Advancedvariable4 {
+    public static void main(String[] args) {
+        int number = 10;
+
+        int square = number * number;
+        System.out.println("The square of " + number + " is: " + square);
+
+    }
+}
