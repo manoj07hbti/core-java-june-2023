6a5a8579203df7472a657c4dd9d50be21bcbbfc7
RamD-1998 <maruti@1998>
2023-07-14 10:42:54 +0530

    advancedvariable1

---
+++ src/variable/Adavncedjavavariable1.java
@@ -0,0 +1,36 @@
+package variable;
+
+public class Adavncedjavavariable1 {
+    public static void main(String[] args) {
+
+     int a=3;
+     int b=9;
+     int sum=a+b;
+     System.out.println("addition of a and b is:"+sum);
+
+     int sub=a-b;
+
+     System.out.println("subtraction is :"+sub);
+
+     int multi=a*b;
+     System.out.println("multi is :"+multi);
+
+     int divide=a/b;
+     System.out.println("division of a and b is : "+divide);
+
+     int i=1;
+     System.out.println("printing is after increement operator"+i);
+
+     i--;
+     System.out.println("decrement value of i is : "+i);
+
+     int x=7;
+     int y=4;
+
+     System.out.println("printing x after minus : "+x);
+
+     x-=y;
+     System.out.println("printing x after minus :"+x);
+
+    }
+}
