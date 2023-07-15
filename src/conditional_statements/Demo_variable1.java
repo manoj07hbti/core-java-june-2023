d1f495848702fd7e086609c34ae3656732b247f0
RamD-1998 <maruti@1998>
2023-07-15 10:07:53 +0530

    if statement age group

---
+++ src/conditional_statements/Demo_variable1.java
@@ -0,0 +1,15 @@
+package conditional_statements;
+
+public class Demo_variable1 {
+    public static void main(String[] args) {
+     int age=19;
+
+     if(age>18){
+         System.out.println("eligible for voting");
+
+     if(age<18){
+     System.out.println("not eligible for voting");
+     }
+     }
+    }
+}
---
+++ src/variable/Advancedvariable5.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Advancedvariable5 {
+    public static void main(String[] args) {
+        int number = 50;
+
+        int square = number * number;
+        System.out.println("The square of " + number + " is: " + square);
+
+    }
+}
