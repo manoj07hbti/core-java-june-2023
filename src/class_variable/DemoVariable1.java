fe9dafb9616a46b62caac08c4e4b49b1fbad5ecf
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-16 18:15:46 +0530

    Variable 1

---
+++ src/class_variable/DemoVariable1.java
@@ -0,0 +1,74 @@
+package class_variable;
+
+public class DemoVariable1 {
+    public static void main(String[] args) {
+        //  SYNTAX : Datatype varName;  variable declaration
+
+        String name="Jatin";
+        System.out.println("This is my Name "+name);
+        String City_Name="Agra";
+        System.out.println("I am From "+City_Name);
+        String City_Name2="Delhi";
+        System.out.println("I live in "+City_Name2);
+        String Study="Java";
+        System.out.println("I am Learning "+Study);
+        String DramaSeries="GOT";
+        System.out.println("My Favourite DramaSeries is "+DramaSeries);
+
+
+        //  SYNTAX : Datatype varName;  variable declaration
+
+        int price=11000;
+        System.out.println("This is my phone Price "+price);
+        int price2=650;
+        System.out.println("This is the Price "+price2+" of Antivirus i bought");
+        int price3=0000;
+        System.out.println("THis is my Laptop Price "+price3);
+        int price4=80;
+        System.out.println("This is my Notebook Price "+price4);
+        int price5=20;
+        System.out.println("This is my Pen Price "+price5);
+
+
+        //  SYNTAX : Datatype varName;  variable declaration
+
+
+        double Price=1.1;
+        System.out.println("This is first items "+Price);
+        double Price2=22222.2;
+        System.out.println("This is second items "+Price2);
+        double Price3=22.22;
+        System.out.println("This is third items "+Price3);
+        double Price4=33.33;
+        System.out.println("This is fourth items "+Price4);
+        double Price5=3.3334445;
+        System.out.println("THis is fifth items "+Price5);
+
+
+        //  SYNTAX : Datatype varName;  variable declaration
+        boolean flag=true;
+        System.out.println("India Won the 2011 ICC WorldCup:"+flag);
+        boolean flag1=true;
+        System.out.println("Argentina Won the 2022 FIFA WorldCup:"+flag1);
+        boolean flag2=true;
+        System.out.println("Brazil has the 5 FIFA title:"+flag2);
+        boolean flag3=false;
+        System.out.println("IS Bond25 is the last 007 movie maybe:"+flag3);
+        boolean flag4=true;
+        System.out.println("Is India will Host the 2023 ICC WorldCup:"+flag4);
+
+
+        String countryName;
+
+        countryName="India";
+
+        System.out.println("My Country Name is:"+countryName);
+
+        countryName="Luxembourg"; // reuse variable and stored new value
+
+        System.out.println("I want to go:"+countryName);
+
+
+
+    }
+}
