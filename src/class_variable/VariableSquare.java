b943f8f0676047592fe1f3f63e4783c8972a5bec
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-16 18:19:59 +0530

    Variable Square

---
+++ src/class_variable/VariableSquare.java
@@ -0,0 +1,57 @@
+package class_variable;
+
+public class VariableSquare {
+
+    public static void main(String[] args) {
+      int num=11;
+      int square =num*num;
+        System.out.println("square of 11: " + square);
+
+        num =12;
+        square =num*num;
+        System.out.println("square of 12: " + square);
+
+        num =13;
+        square =num*num;
+        System.out.println("square of 13: " + square);
+
+        num =14;
+        square =num*num;
+        System.out.println("square of 14: " + square);
+
+        num =15;
+        square =num*num;
+        System.out.println("square of 15: " + square);
+
+        num =16;
+        square =num*num;
+        System.out.println("square of 16: " + square);
+
+        num =17;
+        square =num*num;
+        System.out.println("square of 16: " + square);
+
+        num =18;
+        square =num*num;
+        System.out.println("square of 16: " + square);
+
+        num =19;
+        square =num*num;
+        System.out.println("square of 16: " + square);
+
+        num =20;
+        square =num*num;
+        System.out.println("square of 16: " + square);
+
+
+        int a=1444;
+        int SQUARE =a*a;
+        System.out.println("SQUARE of 1444: " +SQUARE);
+
+        int b=121;
+        int cube =b*b*b;
+        System.out.println(cube);
+
+    }
+    }
+
