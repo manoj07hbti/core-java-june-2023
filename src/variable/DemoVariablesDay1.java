0d0b76ca2494600567a03ba707066d5cace3b4d0
Rohan030723 <rohan301997@gmail.com>
2023-07-17 00:46:58 +0530

    DemoVariablesDay1

---
+++ src/variable/DemoVariablesDay1.java
@@ -0,0 +1,115 @@
+package variable;
+
+  public class DemoVariablesDay1 {
+
+    public static void main(String[] args)
+    {
+   // SYNTAX : Datatype varName; declare a variable
+   // varName = value ; Assignment of data
+   // System.out.println("Message:" +varName)
+
+        String countryName; // We declared a variable countryName with String Datatype
+
+        countryName ="India";
+
+        System.out.println("My Country name is :" +countryName);
+
+
+        int Latitude; // We declared a variable Latitude with int datatype
+
+        Latitude = 3214;
+
+        System.out.println("The India Measurement from N-S is:" +Latitude);
+
+        double Longitude; // We declared a variable Longitude with double datatype
+
+        Longitude = 37.6;
+
+        System.out.println("The India Longitudes from N-S is:" +Longitude);
+
+
+        String History;// We declared a variable History with the String
+
+        History = "Indus Valley Civilization & the Coming of the Aryans";
+
+        System.out.println("Which is the Starting History of India is:" +History);
+
+        int veda; // We declared a variable veda with int datatype
+
+        veda = 4;
+
+        System.out.println("How many veda are there in India is:" +veda);
+
+        double BCE; //We declared a variable BCE with double datatype
+
+        BCE = 3300;
+
+        System.out.println("Indus Civilization Start from:" +BCE);
+
+        String River;
+
+        River = "Ganga";
+
+        System.out.println("Which is the holly river of India:" +River);
+
+        int length;
+
+        length =2525;
+
+        System.out.println("The length of ganga river is:" +length);
+
+        double basinSize;
+
+        basinSize = 1999000;
+
+        System.out.println("The basin Size of River ganga is:" +basinSize);
+
+        String Mountain;
+
+        Mountain = "Himalaya";
+
+        System.out.println( " The highest mountain is:" +Mountain);
+
+        int height;
+
+        height =8848;
+
+        System.out.println("what is the height of the Himalaya is:" +height);
+
+        double lengths;
+
+        lengths = 2400;
+
+        System.out.println("What is the length of it:" +lengths);
+
+        String geography;
+
+        geography = "Asia";
+
+        System.out.println("In which continent India comes:" +geography);
+
+        int Ranked;
+
+        Ranked = 7;
+
+        System.out.println("In asia continent india rankes in:" +Ranked);
+
+        double coastline;
+
+        coastline = 3287263;
+
+        System.out.println("The total coastline of india is:" +coastline);
+
+        boolean flag;
+
+        flag = true;
+
+        System.out.println("All the above data enter is:" +flag);
+
+
+
+
+
+
+    }
+}
