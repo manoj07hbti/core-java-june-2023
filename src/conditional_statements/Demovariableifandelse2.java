8800d14d23a2d698bf2a242328b447a5c408f9b8
RamD-1998 <maruti@1998>
2023-07-16 20:51:44 +0530

    if and else marks and price

---
+++ src/conditional_statements/Demovariableifandelse2.java
@@ -0,0 +1,24 @@
+package conditional_statements;
+
+public class Demovariableifandelse2 {
+    public static void main(String[] args) {
+
+     int marks=60;
+
+     if(marks>60){
+     System.out.println("student pass first division");
+
+     if(marks<33){
+     System.out.println("student not cleared exam");
+
+     int price=5000;
+     if(price>5000){
+     System.out.println("this is a costly product");
+
+     if(price<5000);
+     System.out.println("this product price is average");
+     }
+     }
+     }
+    }
+}
