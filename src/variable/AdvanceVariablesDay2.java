2be81f51dceb5bfae513e7091b6ded11b19b415e
Rohan030723 <rohan301997@gmail.com>
2023-07-17 01:08:26 +0530

    AdvanceVariablesDay2

---
+++ src/variable/AdvanceVariablesDay2.java
@@ -0,0 +1,32 @@
+package variable;
+
+public class AdvanceVariablesDay2 {
+
+    public static void main(String[] args) {
+
+        // write a code for making square of number
+
+        int a=3;
+        int b=9;
+
+        int multi = a*a;
+
+        System.out.println("Square of a and a is:" +multi);
+
+        // write a cube for making cube of number
+
+        int cube = a*a*a;
+
+        System.out.println("Cube of a is:" + cube);
+
+        // write a code to calculate %
+
+        int percentage = 100*10/100;
+
+        System.out.println("Percentage of a is:" +percentage);
+
+
+
+
+    }
+}
