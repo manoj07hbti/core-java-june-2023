17633bc4f0543b14fbcef178d3164720be32d839
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-16 18:21:09 +0530

    Variable Cube

---
+++ src/class_variable/VariableCube.java
@@ -0,0 +1,56 @@
+package class_variable;
+
+public class VariableCube {
+
+    public static void main(String[] args) {
+
+        int num=11;
+        int cube=num*num*num;
+        System.out.println("cube of 11: " + cube);
+
+        num=12;
+        cube=num*num*num;
+        System.out.println("cube of 12: " + cube);
+
+        num=13;
+        cube=num*num*num;
+        System.out.println("cube of 13: " + cube);
+
+        num=14;
+        cube=num*num*num;
+        System.out.println("cube of 14: " + cube);
+
+        num=15;
+        cube=num*num*num;
+        System.out.println("cube of 15: " + cube);
+
+        num=16;
+        cube=num*num*num;
+        System.out.println("cube of 16: " + cube);
+
+        num=17;
+        cube=num*num*num;
+        System.out.println("cube of 17: " + cube);
+
+        num=18;
+        cube=num*num*num;
+        System.out.println("cube of 18: " + cube);
+
+        num=19;
+        cube=num*num*num;
+        System.out.println("cube of 19: " + cube);
+
+        num=20;
+        cube=num*num*num;
+        System.out.println("cube of 20: " + cube);
+
+
+        int b=121;
+        int CUBE =b*b*b;
+        System.out.println(CUBE);
+    }
+
+
+}
+
+
