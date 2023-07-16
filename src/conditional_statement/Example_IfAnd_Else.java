73cb8e230c81f50ec75aae318c1dfaff2b2dddfe
KeertiAbhay <keertikumari900@gmail.com>
2023-07-17 00:13:26 +0530

    Merge remote-tracking branch 'origin/Feature_Keerti' into Feature_Keerti

---
+++ src/conditional_statement/Example_IfAnd_Else.java
@@ -0,0 +1,22 @@
+package conditional_statement;
+
+public class Example_IfAnd_Else {
+
+    public static void main(String args[]) {
+
+        int marks = 60;
+        if (marks >= 50) {
+
+            System.out.println("You are passed with first division");
+        } else {
+            System.out.println("You are not passed with first division");
+        }
+
+        int marks1 = 33;
+        if (marks1 < 30) {
+
+            System.out.println("You are cleared exams:");
+        }else {
+            System.out.println("You are not cleared exams:");
+
+    }   } }
---
+++ src/maths_variable/Variable_Day_3.java
@@ -0,0 +1,37 @@
+package maths_variable;
+
+public class Variable_Day_3 {
+
+    public static <sum> void main(String [] args){
+        int a =8;
+        int b =6;
+        a+=b;
+        System.out.println("printing addition ----->"+a);
+
+        a-=b;
+        System.out.println("printing subtraction ------>"+b);
+
+        a*=b;
+        System.out.println("Printing multiple ------>"+a);
+
+        int c= 5;
+        int square2 = c*c;
+        System.out.println("printing square  :"+square2);
+
+        int c1 = 4;
+        int cube3=4*4*4;
+        System.out.println("Printing cube :"+cube3);
+
+        int i = 5;
+        i++;
+        System.out.println("Printing is after increment: "+i);
+
+        i--;
+        System.out.println("Printing Decrement value of is :"+i);
+
+
+
+
+    }
+}
+ 
