9f82e74d87f1eeff8c02f93c2147c05f9a4f1f02
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-17 12:44:29 +0530

    IF and ELSE Assignment COMMIT

---
+++ src/class_conditional_statements/IFandELSE4.java
@@ -0,0 +1,25 @@
+package class_conditional_statements;
+
+public class IFandELSE4 {
+
+    public static void main(String[] args) {
+        //SYNTAX : if(condition){ code to be executed } else {}
+
+        int marks = 57;
+        if (marks >= 60) {
+
+            System.out.println("First Division");
+        } else {
+            System.out.println("Not Cleared Exam");
+        }
+        //SYNTAX : if(condition){ code to be executed } else {}
+        int marks1 = 34;
+        if (marks1 >= 33) {
+
+            System.out.println("Cleared Exam");
+        } else {
+            System.out.println("Not Cleared Exam");
+        }
+
+    }
+}
