428ff2dbf9fdd0424d6fe282c89f99d685efca11
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-17 10:42:05 +0530

    Percentage variable

---
+++ src/class_variable/VariablePercentage.java
@@ -0,0 +1,49 @@
+package class_variable;
+
+public class VariablePercentage {
+    public static void main(String[] args) {
+
+        double num=1000;
+        double percentage=1000*10/100;
+        System.out.println("Percentage of 1000: "+percentage);
+
+        num=2000;
+        percentage=2000*10/100;
+        System.out.println("Percentage of 2000: "+percentage);
+
+        num=3000;
+        percentage=3000*10/100;
+        System.out.println("Percentage of 3000: "+percentage);
+
+        num=4000;
+        percentage=4000*10/100;
+        System.out.println("Percentage of 4000: "+percentage);
+
+        num=5000;
+        percentage=5000*10/100;
+        System.out.println("Percentage of 5000: "+percentage);
+
+        num=6000;
+        percentage=6000*10/100;
+        System.out.println("Percentage of 6000: "+percentage);
+
+        num=7000;
+        percentage=7000*10/100;
+        System.out.println("Percentage of 7000: "+percentage);
+
+        num=8000;
+        percentage=8000*10/100;
+        System.out.println("Percentage of 8000: "+percentage);
+
+        num=9000;
+        percentage=9000*10/100;
+        System.out.println("Percentage of 9000: "+percentage);
+
+        num=10000;
+        percentage=10000*10/100;
+        System.out.println("Percentage of 10000: "+percentage);
+
+
+
+    }
+}
