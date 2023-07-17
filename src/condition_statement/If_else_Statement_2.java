8ed2548564b4202aab6f247927e1bc72e74f0d78
pcz <bachchoomathur3@gmail.com>
2023-07-17 12:16:13 +0530

    Commit IF and else Assignment Second

---
+++ src/condition_statement/If_else_Statement_2.java
@@ -0,0 +1,35 @@
+package condition_statement;
+
+public class If_else_Statement_2 {
+    public static void main(String[] args) {
+        int a=30, b=60;
+        if (a>b){
+            System.out.println("Printing Number a : "  +a);
+        }else {
+            System.out.println("Printing Number b : "  +b);
+        }
+        if (50>60){
+            System.out.println("Printing Short number : ");
+        }else {
+            System.out.println("Printing longer number : ");
+
+        }
+        String c = "Agra";
+        String d= "Agra";
+        if (c==d){
+            System.out.println("Printing true");
+        }else {
+            System.out.println("Printing false");
+        }
+        String E=new String("Tundla");
+        String F=new String("Tundla");
+        if (E==F){
+            System.out.println("Printing true");
+        } else {
+            System.out.println("Printing false");
+
+        }
+
+
+    }
+}
