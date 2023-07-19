7d39e84d54c41b01b589ce295653718bbea1bb88
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-19 10:15:19 +0530

    NESTED IF COMMIT

---
+++ src/class_conditional_statements/NestedIf.java
@@ -0,0 +1,46 @@
+package class_conditional_statements;
+
+import java.sql.SQLOutput;
+
+public class NestedIf {
+
+    public static void main(String[] args) {
+
+        int num = 30;
+
+        if (num > 0) {
+
+            if (num % 2 == 0) {
+
+                System.out.println("Number is Even: " + num);
+            } else {
+
+                System.out.println("Number is Odd: " + num);
+            }
+
+
+        }else{
+
+            System.out.println("Please Enter Valid Number");
+        }
+
+
+
+
+
+
+        int age =-1;
+        if (age > 0) {
+
+            if(age>=18){
+                System.out.println("Eligible for Casting the Vote");
+            }else {
+                System.out.println("Not Eligible for Casting the Vote");
+            }
+        }else System.out.println("Please Enter the Valid Age");
+        }
+        }
+
+
+
+
