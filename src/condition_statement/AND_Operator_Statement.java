2478d88c323f4a054ebab0f0ef49ef9ea1f5e2d0
pcz <bachchoomathur3@gmail.com>
2023-07-19 15:08:00 +0530

    Commit AND Operator Statement update Program Voter.

--- src/condition_statement/AND_Operator_Statement.java
+++ src/condition_statement/AND_Operator_Statement.java
@@ -29,8 +29,38 @@ public class AND_Operator_Statement {
 
         }else {
             System.out.println("Printing Maths Marks and Science Arts are false");
+        }
+        int age=18;
+        String State="Uttar Pradesh";
+        if(age>=18  &&  State=="Uttar Pradesh"){
+            System.out.println(" Voter are eligible for Uttar Pradesh  ");
+        }else {
+            System.out.println(" Voter are not eligible for Uttar Pradesh  ");
 
+        }State="Delhi";
+        if (age>=18 && State=="Uttar Pradesh") {
+            System.out.println(" Voter are eligible for Uttar Pradesh  ");
+
+        } else {
+            System.out.println(" Voter are not eligible for Uttar Pradesh  ");
 
         }
+        age = 15;
+        if (age>=18 && State=="Uttar Pradesh"){
+            System.out.println(" Voter are eligible for Uttar Pradesh  ");
+
+        }else {
+            System.out.println(" Voter are not eligible for Uttar Pradesh  ");
+
+        } age = 17;
+        State="Pune";
+        if (age>=18 && State=="Uttar Pradesh"){
+            System.out.println(" Voter are eligible for Uttar Pradesh  ");
+
+        }else {
+            System.out.println(" Voter are not eligible for Uttar Pradesh  ");
+
+        }
+
     }
 }
