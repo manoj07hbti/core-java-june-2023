a4bd7ec3dea9458771de3817c2f5061af978fea6
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-19 20:08:53 +0530

    Nested   if  statement

---
+++ src/conditional_statements/Nested_if.java
@@ -0,0 +1,66 @@
+package conditional_statements;
+
+public class Nested_if {
+
+    public static void main(String[] args) {
+        // odd number
+        int number = 5;
+
+        if (number > 0) {
+            if (number % 2 == 0) {
+
+                System.out.println(" the value of number is even : " + number);
+
+            } else {
+                System.out.println(" the value of number is ood : " + number);
+            }
+        } else {
+            System.out.println(" the value of enter the valid number : " + number);
+        }
+        number = 4;
+
+        if (number > 0) {
+            if (number % 2 == 0) {
+                System.out.println("the value of number if even : " + number);
+            } else {
+                System.out.println(" the value of number is odd : " + number);
+            }
+        } else {
+            System.out.println(" Enter the valid number ");
+        }
+        int age = 10;
+        if (age > 0) {
+            if (age >= 18) {
+                System.out.println("person is eligible for voting : " + age);
+
+            } else {
+                System.out.println("person is not eligible for voting : " + age);
+            }
+        }else{
+            System.out.println("enter the valid number " );
+
+        }
+        age=22;
+        if(age>0) {
+            if (age >= 18) {
+                System.out.println("person is eligible for voting : " + age);
+            } else {
+                System.out.println("person is not eligible for voting : " + age);
+
+
+            }
+        }else{
+            System.out.println("Enter te valid number");
+        }
+        age=-10;
+        if(age>0){
+            if(age>18){
+                System.out.println("person is eligile for voting : " +age);
+            }else{
+                System.out.println("person is not eligible for voting : " + age);
+            }
+        }else{
+            System.out.println("Enter the valid number");
+        }
+    }
+}
---
+++ src/practicjava/Practice.java
@@ -0,0 +1,46 @@
+package practicjava;
+
+public class Practice {
+    public static void main(String[] args) {
+        int number=12;
+        int square=number*number;
+        System.out.println("The value of  square " + number + " is : "    + square);
+
+         number=9;
+         int cube=number*number*number;
+        System.out.println("The value of  cube " + number + " is : " + cube);
+
+        double amount=100000;
+        double percentage =100000*5/100;
+        System.out.println("  5 % of   " + amount + " is : " + percentage);
+
+        amount =20000;
+         percentage =20000*5/100;
+        System.out.println("5 % of " + amount + " is : " + percentage);
+
+        amount=5000;
+        percentage=5000*2/100;
+        System.out.println("2 % of " + amount + " is : " + percentage);
+
+        number=2;
+        cube=number*number*number;
+        System.out.println("The cube of " + number + " is  : " + cube);
+
+        number =5;
+        square=number*number;
+        System.out.println(" The square of " + number + " is : " + square);
+
+        int age ;
+        age =45;
+        System.out.println("age of the person is : " + age );
+
+        String city;
+        city = " Aurangabad ";
+        System.out.println(" person is belongs from : " + city);
+
+        double price;
+        price=49.99999;
+        System.out.println("price of the product is : "  + price);
+
+    }
+}
