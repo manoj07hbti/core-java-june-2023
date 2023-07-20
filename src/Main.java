b5197de190a29f78ccd990abdb154b17ca929bb2
Uday6667 <yadavuday63@gmail.com>
2023-07-20 21:17:11 +0530

    my && operates assingment is completed
    Signed-off-by: Uday6667 <yadavuday63@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,65 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int age = 17;
+
+        if (age < 18) {
+
+            System.out.println(" print ellegebal for voting");
+        } else {
+
+            System.out.println("person not ellegebal for voting");
+
+        }
+        if (age < 18) {
+
+            System.out.println("print person not ellegebal for voting");
+        } else {
+            System.out.println("print person ellegebal for voting");
+        }
+        int marks = 55;
+
+        if (marks > 60) {
+
+            System.out.println("marks grater then 60 student pass out frist devesion ");
+        } else {
+            System.out.println("marks less then 60 student pass second devesin");
+        }
+        marks = 35;
+
+        if (marks > 33) {
+
+            System.out.println("print student pass 3rd devesion");
+
+        } else {
+            System.out.println("print student is not pass");
+        }
+        int price = 5000;
+
+        if (price >= 5000) {
+
+            System.out.println("print price is high");
+        } else {
+            System.out.println("price is low");
+        }
+        String countryname = "india";
+
+        if (countryname == "india") {
+
+            System.out.println("print person bellong to india");
+        } else {
+            System.out.println("print person not bellong to india");
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
---
+++ src/operete_rin_java/Fristoperater.java
@@ -0,0 +1,92 @@
+package operete_rin_java;
+
+public class Fristoperater {
+
+    public static void main(String[] args) {
+
+
+        // TRUE/TRUE OPERATER
+
+        int marks = 60;
+        String Subject = "science";
+        if (marks >= 60 && Subject == "science") {
+
+
+            System.out.println("TRUE student is science baground");
+        } else {
+            System.out.println(" student is not science baground");
+        }
+// TRUE AND FALSE STATMENT
+
+        Subject = "science";
+        if (marks < 55 && Subject == "science") {
+
+
+            System.out.println("TRUE:student is science baground");
+        } else {
+            System.out.println("false: student is not science baground");
+        }
+
+// FALSE AND TRUE STATMENT
+        marks = 60;
+        if (marks >= 60 && Subject == "art") {
+
+
+            System.out.println("TRUE:student is science baground");
+        } else {
+            System.out.println("false: student is not science baground");
+        }
+// FALSE && FALSE STATMENT
+
+        Subject = "art";
+        marks = 60;
+        if (marks < 60 && Subject == "art") {
+
+            System.out.println("TRUE:student is science baground");
+        } else {
+            System.out.println("false: student is not science baground");
+        }
+
+        // TRUE && TRUE = TRUE
+
+        int age = 18;
+        String State = "uttar pradesh";
+        if (State == "uttar pradesh" && age >= 18) {
+
+            System.out.println("TRUE: Person ellegibal for voting in uttar pradesh");
+
+        } else {
+            System.out.println("FALSE:Person is not ellegebal for voting");
+        }
+// TRUE && FALSE = FALSE
+        age = 18;
+        State = "uttar pradesh";
+        if (State == "maharastra" && age >= 18) {
+
+            System.out.println("TRUE: Person ellegibal for voting in uttar pradesh");
+
+        } else {
+            System.out.println("FALSE:Person is not ellegebal for voting");
+        }
+        // FALSE && TRUE = FALSE
+        age = 18;
+        State = "uttar pradesh";
+        if (State == "uutar pradesh" && age < 18) {
+
+            System.out.println("TRUE: Person ellegibal for voting in uttar pradesh");
+
+        } else {
+            System.out.println("FALSE:Person is not ellegebal for voting");
+        }
+        //FALSE && FALSE = FALSE
+        age = 18;
+        State = "uttar pradesh";
+        if (State == "maharastra" && age < 18) {
+
+            System.out.println("TRUE: Person ellegibal for voting in uttar pradesh");
+
+        } else {
+            System.out.println("FALSE:Person is not ellegebal for voting");
+        }
+    }
+}
