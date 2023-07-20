c6779bb523f963787da3870b36a22534241a941a
Anoopk2021 <anoop.k.2017@gmail.com>
2023-07-20 13:23:39 +0530

    AssignmentIfElse

---
+++ src/conditional_statements/AssignmentORandNotOperator.java
@@ -0,0 +1,15 @@
+package conditional_statements;
+
+public class AssignmentORandNotOperator {
+    public static void main(String[] args) {
+//Write a code which can tell any positive number is devided by 3 or not .
+        int number = 18;
+        if (number > 3) {
+            if (number / 27 == 3) {
+                System.out.println("Number is devided by 3");
+            } else {
+                System.out.println("Number is not devided by 3");
+            }
+        }
+    }
+}
