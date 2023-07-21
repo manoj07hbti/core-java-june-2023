522617974f29cc039ae43154924a392db17dafce
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-21 19:05:46 +0530

    my NotOperator class

---
+++ src/conditional_package/DemoNotOperator.java
@@ -0,0 +1,19 @@
+package conditional_package;
+
+public class DemoNotOperator {
+    public static void main(String[] args) {
+
+        String city = "mumbai";
+        if (!(city != "Firozabad"))
+        {
+            System.out.println("you belongs to firozabad....");
+        }else{
+            System.out.println("you do not belongs to Firozabad....");
+
+        }
+
+
+
+    }
+}
+
