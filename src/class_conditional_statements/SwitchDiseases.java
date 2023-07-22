4a0ce13d085d121f0f5521b9f8422deca4427a0e
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-22 15:20:36 +0530

    SWITCH DISEASES COMMIT 2

---
+++ src/class_conditional_statements/SwitchDiseases.java
@@ -0,0 +1,81 @@
+package class_conditional_statements;
+
+public class SwitchDiseases {
+
+    public static void main(String[] args) {
+
+
+        int diseases =3;
+
+        switch (diseases){
+            case 1:
+                System.out.println("Cough");
+                break;
+
+            case 2:
+                System.out.println("Typhoid");
+                break;
+
+            case 3:
+                System.out.println("Tuberculosis");
+                break;
+
+            case 4:
+                System.out.println("Cholera");
+                break;
+
+            case 5:
+                System.out.println("Pneumonia");
+                break;
+
+            case 6:
+                System.out.println("Diphtheria");
+                break;
+
+            case 7:
+                System.out.println("Leprosy");
+                break;
+
+            case 8:
+                System.out.println("Bubonic Plague");
+                break;
+
+            case 9:
+                System.out.println("Gonorrhoea");
+                break;
+
+            case 10:
+                System.out.println("Tetanus");
+                break;
+
+            case 11:
+                System.out.println("Anthrax");
+                break;
+
+            case 12:
+                System.out.println("Hepatitis");
+                break;
+
+            case 13:
+                System.out.println("Poliomyelitis");
+                break;
+
+            case 14:
+                System.out.println("Measles");
+                break;
+
+            case 15:
+                System.out.println("Dengue");
+                break;
+
+            case 16:
+                System.out.println("Myastheniagrabis");
+                break;
+
+            default:
+                System.out.println("Please Enter right Information");
+
+        }
+
+    }
+}
