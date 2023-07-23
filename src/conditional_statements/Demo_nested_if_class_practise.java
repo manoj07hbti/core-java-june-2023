932907dd3fe7d8278f7631d7f2746756f378e058
RamD-1998 <maruti@1998>
2023-07-23 17:00:37 +0530

    Switch statement assignment

---
+++ src/conditional_statements/Demo_nested_if_class_practise.java
@@ -0,0 +1,107 @@
+package conditional_statements;
+
+public class Demo_nested_if_class_practise {
+    public static void main(String[] args) {
+        int num = 30;
+        if (num > 0) {
+            if (num % 3 == 0) {
+                System.out.println("number is divisible by 3: " + num);
+            } else {
+                System.out.println("number is not divisible by 3: " + num);
+            }
+        } else {
+            System.out.println("please enter valid number");
+        }
+        num = 90;
+        if (num > 0) {
+            if (num % 9 == 0) {
+                System.out.println("number is divisible by 9 : " + num);
+            } else {
+                System.out.println("number is not divisible by 9 : " + num);
+            }
+        } else {
+            System.out.println("please enter a valid number");
+        }
+        num = 100;
+        if (num > 0) {
+            if (num % 10 == 0) {
+                System.out.println("number is divisible by 10 : " + num);
+            } else {
+                System.out.println("number is not divisible by 10 : " + num);
+            }
+        } else {
+            System.out.println("please enter a valid number");
+        }
+        num = 130;
+        if (num > 0) {
+            if (num % 13 == 0) {
+                System.out.println("number is divided by 13 : " + num);
+            } else {
+                System.out.println("number is not divided by 13 : " + num);
+            }
+        } else {
+            System.out.println("please enter a valid number");
+        }
+        num = 190;
+        if (num > 0) {
+            if (num % 19 == 0) {
+                System.out.println("number is divisible by 19 : " + num);
+            } else {
+                System.out.println("number is not divisible by 19 : " + num);
+            }
+            } else {
+            System.out.println("please enter a valid number");
+        }
+        num = 210;
+        if ( num > 0) {
+            if (num % 21 == 0) {
+                System.out.println("number is divided by 21 : " + num);
+            } else {
+                System.out.println("number is not divided by 21 : " + num);
+            }
+        } else {
+            System.out.println("please enter a valid number ");
+        }
+        num = 510;
+        if ( num > 0) {
+            if (num % 51 == 0) {
+                System.out.println("number is divisible by 51 : " + num);
+            } else {
+                System.out.println("number is not divisible by 51 : " + num);
+            }
+            } else{
+                System.out.println("plaese enter a valid number");
+            }
+        num = 610;
+        if ( num > 0) {
+            if (num % 61 == 0) {
+                System.out.println("number is divisible by 61 : " + num);
+            } else {
+                System.out.println("number is not divisible by 61 : " + num);
+            }
+        } else {
+            System.out.println("please enter a valid number");
+        }
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/conditional_statements/Demo_switch.java
@@ -0,0 +1,72 @@
+package conditional_statements;
+
+public class Demo_switch {
+    public static void main(String[] args) {
+
+        int month=11;
+        /*  if(day==1){
+            System.out.println("Monday..");
+        }else if(day==2){
+            System.out.println("Tuesday..");
+        }else if(day==3){
+            System.out.println("Wednesday..");
+        }else if(day==4){
+            System.out.println("Thursday..");
+        }else if(day==5){
+            System.out.println("Friday..");
+        }else if(day==6){
+            System.out.println("Saturday..");
+        }else if(day==7){
+            System.out.println("Sunday..");
+
+
+         */
+        switch (month) {
+            case 1:
+                System.out.println("JANUARY");
+                break;
+            case 2:
+                System.out.println("FEBRUARY");
+                break;
+            case 3:
+                System.out.println("MARCH");
+                break;
+            case 4:
+                System.out.println("APRIL");
+                break;
+            case 5:
+                System.out.println("MAY");
+                break;
+            case 6:
+                System.out.println("JUNE");
+                break;
+            case 7:
+                System.out.println("JULY");
+                break;
+            case 8:
+                System.out.println("AUGUST");
+                break;
+            case 9:
+                System.out.println("SEPTEMBER");
+                break;
+            case 10:
+                System.out.println("OCTOBER");
+                break;
+            case 11:
+                System.out.println("NOVEMBER");
+                break;
+            case 12:
+                System.out.println("DECEMBER");
+                break;
+
+            default:
+                System.out.println("Please enter correct Month");
+
+        }
+
+
+        }
+
+        }
+
+
