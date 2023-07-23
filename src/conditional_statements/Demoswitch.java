640e0683b9cb93942f7f8757e6dde27d5b459991
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-23 19:09:50 +0530

    Assignment of switch

---
+++ src/conditional_statements/Demoswitch.java
@@ -0,0 +1,59 @@
+package conditional_statements;
+
+public class Demoswitch {
+    public static void main(String[] args) {
+          int month=1;
+
+
+          switch (month){
+              case 1:
+                  System.out.println("january");
+              break;
+
+              case 2:
+                  System.out.println("February");
+                  break;
+
+              case 3:
+                  System.out.println("March");
+                  break;
+
+              case 4:
+                  System.out.println("April");
+                  break;
+
+              case 5:
+                  System.out.println("may");
+                  break;
+
+              case 6:
+                  System.out.println("june");
+                  break;
+
+              case 7:
+                  System.out.println("july");
+                  break;
+
+              case 8:
+                  System.out.println("August");
+                  break;
+
+              case 9:
+                  System.out.println("september");
+                  break;
+
+              case 10:
+                  System.out.println("october");
+                  break;
+
+              case 11:
+                  System.out.println("November");
+                  break;
+
+              case 12:
+                  System.out.println("December");
+                  break;
+
+          }
+    }
+}
