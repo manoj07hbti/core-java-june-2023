cc86c9fa3700e422c924071cb44be9e24f009158
Anoopk2021 <anoop.k.2017@gmail.com>
2023-07-25 00:00:18 +0530

    Assignment_Loop_Even

---
+++ src/loop_working/Assignment_Loop_Even.java
@@ -0,0 +1,17 @@
+package loop_working;
+
+public class Assignment_Loop_Even {
+
+    public static void main(String[] args) {
+        //Print all even number between 1 to 100 using for
+        //Print all odd number between 1 to 100  using for
+           int n =100;
+        for (int i=0; i<=n; i++){
+            if ( i%2 == 0){
+                System.out.println(i);
+
+            }
+
+        }
+    }
+}
