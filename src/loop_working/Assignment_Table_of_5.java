febeca5c996e17a14dea3e9e9649da649e55087f
Anoopk2021 <anoop.k.2017@gmail.com>
2023-07-25 00:01:13 +0530

    Assignment_Table_of_5

---
+++ src/loop_working/Assignment_Table_of_5.java
@@ -0,0 +1,12 @@
+package loop_working;
+
+public class Assignment_Table_of_5 {
+    public static void main(String[] args) {
+        int number = 5;
+        for (int i=1; i<=10; i++){
+            System.out.println(number + " X " +i+ " = " + number*i);
+        }
+    }
+
+
+}
