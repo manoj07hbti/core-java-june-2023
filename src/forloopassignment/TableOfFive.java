ef95631e9cdeace92e822f1628157ad21b2a5911
Ankita <ankitachougule19@gmail.com>
2023-07-24 21:54:40 +0530

    forlooptable5

---
+++ src/forloopassignment/TableOfFive.java
@@ -0,0 +1,20 @@
+package forloopassignment;
+
+public class TableOfFive {
+
+
+    public static void main(String[] args) {
+
+        int number=5;
+
+        /*SYNTAX FOR INCREMENT FOR LOOP
+            for(start_condition; expression; increment )
+         */
+
+        for (int i=1; i<=10; i++)
+        {
+            System.out.println(number +"*"+i +"="+number*i);
+
+        }
+    }
+}
