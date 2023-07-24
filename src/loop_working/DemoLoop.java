3579e871dc8e64e16befdbb88c51bd5cc917e60e
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-07-24 21:26:39 +0530

    for loop demo

---
+++ src/loop_working/DemoLoop.java
@@ -0,0 +1,33 @@
+package loop_working;
+
+public class DemoLoop {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+/*  SYNTAX: INCREMENT FOR LOOP
+        for (start_condition; expression; increment){
+            //CODE
+        }*/
+
+        for(int i=1; i<=5; i++){
+
+            System.out.println("Hello inside for Loop..."+i);
+        }
+/*  SYNTAX: DECREMENT FOR LOOP
+        for (start_condition; expression; decrement){
+            //CODE
+        }*/
+
+        for(int i=10; i>0 ;i--){
+
+            System.out.println("Decrement for loop: "+i);
+        }
+
+
+    }
+}
---
+++ src/loop_working/InfiniteLoop.java
@@ -0,0 +1,21 @@
+package loop_working;
+
+public class InfiniteLoop {
+
+    public static void main(String[] args) {
+
+
+
+        for(int i=1; i>0; i++){
+
+            System.out.println("Infinite loop :"+i);
+        }
+
+        for(int i=-1; i<0; i--){
+
+            System.out.println("Infinite loop :"+i);
+        }
+
+
+    }
+}
