8c71d3ffaf3bfa352a6b26428853cd1f9987a9da
Anoopk2021 <anoop.k.2017@gmail.com>
2023-07-24 23:20:48 +0530

    Loop_Concept

---
+++ src/loop_working/Table_of_3.java
@@ -0,0 +1,11 @@
+package loop_working;
+
+public class Table_of_3 {
+    public static void main(String[] args) {
+
+            int number=3;
+            for(int i=1; i<=10; i++){
+                System.out.println(number + " X " +i+ " = "+number*i);
+            }
+    }
+}
