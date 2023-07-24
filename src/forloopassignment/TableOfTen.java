371836145049666616ff255cb2e7288c016fbfaa
Ankita <ankitachougule19@gmail.com>
2023-07-24 21:55:10 +0530

    forlooptable10

---
+++ src/forloopassignment/TableOfTen.java
@@ -0,0 +1,14 @@
+package forloopassignment;
+
+public class TableOfTen {
+    public static void main(String[] args) {
+
+
+        int number = 10;
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(number + " X " +i +" = " +number*i);
+        }
+    }
+}
