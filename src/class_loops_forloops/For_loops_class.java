eb90ba4909c0949dd08b7c4dd3134dd98425cc28
Uday6667 <yadavuday63@gmail.com>
2023-07-24 23:39:05 +0530

    my for loops 5&10 assingment is completed

---
+++ src/class_loops_forloops/For_loops_class.java
@@ -0,0 +1,33 @@
+package class_loops_forloops;
+
+public class For_loops_class {
+
+    public static void main(String[] args) {
+
+
+        int number = 5;
+        for (int i=1; i<=10; i++){
+
+            System.out.println(number+"X" +i+ "=" +number*i);
+        }
+        number = 10;
+        for (int i=1; i<=10; i++){
+
+            System.out.println(number+"X" +i+ "=" +number*i);
+        }
+number=4;
+
+        for (int i=1; i<=10; i++){
+
+            System.out.println(number +"X"+i+"="+ number*i);
+        }
+
+
+
+    }
+
+
+}
+
+
+
