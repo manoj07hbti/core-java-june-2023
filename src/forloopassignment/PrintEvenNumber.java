4508011e78bef457405f88fd8399c6395008193d
Ankita <ankitachougule19@gmail.com>
2023-07-24 22:41:37 +0530

    EvenOddNumber

---
+++ src/forloopassignment/PrintEvenNumber.java
@@ -0,0 +1,17 @@
+package forloopassignment;
+
+public class PrintEvenNumber {
+    public static void main(String[] args) {
+        int number=1;
+        for (int i=1;i<=100;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println("Even No is  "+i);
+            }
+
+
+
+        }
+    }
+}
---
+++ src/forloopassignment/PrintOddNumber.java
@@ -0,0 +1,18 @@
+package forloopassignment;
+
+public class PrintOddNumber
+{
+    public static void main(String[] args) {
+        int number=1;
+
+        for (int i=1;i<=100;i++)
+        {
+            if(i%2==1)
+            {
+                System.out.println("Number is odd  "+i);
+            }
+
+        }
+    }
+
+}
