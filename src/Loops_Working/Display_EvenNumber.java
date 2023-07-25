4fa1a77f6397bf3b2588193e2ce1af97af9f64db
pcz <bachchoomathur3@gmail.com>
2023-07-25 09:09:13 +0530

    Commit Even Number :

---
+++ src/Loops_Working/Display_EvenNumber.java
@@ -0,0 +1,12 @@
+package Loops_Working;
+
+public class Display_EvenNumber {
+    public static void main(String[] args) {
+        int Number=100;
+        for (int i=1;i<=100;i++){
+            if (i%2==0){
+                System.out.println("Printing Only Even Number is  ---> "  +i);
+            }
+        }
+    }
+}
