a992f5b26577fc6da6cf86d33693b39d88a99f93
pcz <bachchoomathur3@gmail.com>
2023-07-25 20:12:08 +0530

    Commit Odd Number :

---
+++ src/July_25_2023_test/Odd_Number.java
@@ -0,0 +1,12 @@
+package July_25_2023_test;
+
+public class Odd_Number {
+    public static void main(String[] args) {
+        int number=300;
+        for (int i=1;i<=300;i++){
+            if (i%3==1){
+                System.out.println("Printing Odd number is : "  +i);
+            }
+        }
+    }
+}
