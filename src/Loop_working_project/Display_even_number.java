c46d4d45efd74322de8f427a835770e073cb7a80
RamD-1998 <maruti@1998>
2023-07-25 10:19:44 +0530

    even number loop

---
+++ src/Loop_working_project/Display_even_number.java
@@ -0,0 +1,13 @@
+package Loop_working_project;
+
+public class Display_even_number {
+    public static void main(String[] args) {
+
+     int number=100;
+     for (int i=1; i<=100;i++){
+         if(i%2==0){
+             System.out.println("printing only even numbers" +i);
+         }
+     }
+    }
+}
