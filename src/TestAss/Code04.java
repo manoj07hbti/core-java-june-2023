93d4db683e5b1aa57dcaa3ba4ec3d4e50edb8cf9
Rohan030723 <rohan301997@gmail.com>
2023-07-25 20:18:48 +0530

    Code04

---
+++ src/TestAss/Code04.java
@@ -0,0 +1,11 @@
+package TestAss;
+
+public class Code04 {
+
+    public static void main(String[] args) {
+
+        int percentage=5%100*500;
+
+        System.out.println("Percentage of:" +percentage);
+    }
+}
