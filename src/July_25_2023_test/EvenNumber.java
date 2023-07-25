41cd5279007a7677af6524fe85e954e0a138de97
pcz <bachchoomathur3@gmail.com>
2023-07-25 20:07:09 +0530

    Commit even Number :

---
+++ src/July_25_2023_test/EvenNumber.java
@@ -0,0 +1,17 @@
+package July_25_2023_test;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        int number=300;
+        for (int i=1; i<=300;i++){
+            if (i%3==0){
+                System.out.println("Printing Number is even : " +i );
+            }
+                }
+            }
+
+        }
+
+
+
+
