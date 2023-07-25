3fbaba3e831f39a72966e8551c1da45522f77c09
Rohan030723 <rohan301997@gmail.com>
2023-07-25 20:09:07 +0530

    Code02

---
+++ src/TestAss/Code02.java
@@ -0,0 +1,19 @@
+package TestAss;
+
+public class Code02 {
+
+    public static void main(String[] args) {
+// write a code to make a factorial of 4
+        int a=4;
+        int b=3;
+        int c=2;
+        int d=1;
+
+        int multi = a*b*c*d;
+
+        System.out.println("Multiply a*b*c*d:" +multi);
+
+    }
+
+
+}
