f99122809fc71602ea956e7989a3be713649cb0d
RamD-1998 <maruti@1998>
2023-07-25 09:48:56 +0530

    Tabletenloop

---
+++ src/Loop_working_project/Table_of_ten.java
@@ -0,0 +1,11 @@
+package Loop_working_project;
+
+public class Table_of_ten {
+    public static void main(String[] args) {
+
+        int number=10;
+        for (int i=1; i<=10; i++){
+            System.out.println(number +"x"+i+" = "+ number*i );
+        }
+    }
+}
