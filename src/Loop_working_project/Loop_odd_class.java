0c4a0e245f15f750862464bd16aee3bd8f80228e
RamD-1998 <maruti@1998>
2023-07-25 10:34:46 +0530

    Odd Number loop assignment

---
+++ src/Loop_working_project/Loop_odd_class.java
@@ -0,0 +1,12 @@
+package Loop_working_project;
+
+public class Loop_odd_class {
+    public static void main(String[] args) {
+        int number=100;
+        for(int i=1;i<=100;++i){
+            if(i%2==1){
+                System.out.println("printing all odd numbers" +i);
+            }
+        }
+    }
+}
