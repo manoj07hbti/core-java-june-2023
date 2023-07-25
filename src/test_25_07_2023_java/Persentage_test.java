49177e1e6b7e9dd30413f2dfc6d19ddffe144f89
Uday6667 <yadavuday63@gmail.com>
2023-07-25 21:34:08 +0530

    my pesentage value assingment is completed

---
+++ src/test_25_07_2023_java/Persentage_test.java
@@ -0,0 +1,12 @@
+package test_25_07_2023_java;
+
+public class Persentage_test {
+
+    public static void main(String[] args) {
+        int number=500;
+
+        int persentage= 500 * 5 / 100;
+
+        System.out.println("printing the value of 500 by 5 persentage" +"=" +persentage);
+    }
+}
