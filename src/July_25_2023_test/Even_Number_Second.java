10a10ab5779a3ad1262a11a410118e4f425ce1fe
pcz <bachchoomathur3@gmail.com>
2023-07-25 20:33:27 +0530

    Commit Even Number is 100  :

---
+++ src/July_25_2023_test/Even_Number_Second.java
@@ -0,0 +1,12 @@
+package July_25_2023_test;
+
+public class Even_Number_Second {
+    public static void main(String[] args) {
+        int number=100;
+        for (int i=1; i<=100;i++){
+            if (i%2==0){
+                System.out.println("Printing Even number is : " +i);
+            }
+        }
+    }
+}
