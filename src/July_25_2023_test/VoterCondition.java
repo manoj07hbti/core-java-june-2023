6dbfaccfc3bc80b1d4dcef62555fd1d11135ce09
pcz <bachchoomathur3@gmail.com>
2023-07-25 20:29:08 +0530

    Commit Voter is only pune eligible  :

---
+++ src/July_25_2023_test/VoterCondition.java
@@ -0,0 +1,15 @@
+package July_25_2023_test;
+
+public class VoterCondition {
+    public static void main(String[] args) {
+        int age=18;
+        String Voter="Pune";
+        if (Voter=="Pune"){
+            System.out.println("Printing eligible is only Pune : " );
+        }else {
+            System.out.println("Printing eligible is not only Pune : " );
+
+        }
+
+    }
+}
