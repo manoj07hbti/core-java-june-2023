588575d38fa1af209adf4bd3dbda98ae6046ef41
Uday6667 <yadavuday63@gmail.com>
2023-07-25 22:11:51 +0530

    my even odd assingment is completed

---
+++ src/test_25_07_2023_java/Even_number.java
@@ -0,0 +1,24 @@
+package test_25_07_2023_java;
+
+public class Even_number {
+
+    public static void main(String[] args) {
+
+        int number=1;
+
+        for (int i=1; i<100; i++){
+
+            if (i %2==0){
+
+                System.out.println("print even number"+i);
+            }
+            number=300;
+            for (i=1; i<=300; i++){
+
+                if (i%3==0){
+                    System.out.println("print odd number"+i);
+                }
+            }
+        }
+    }
+}
