9806f715c76ade51b2588e7838de780860538a60
pcz <bachchoomathur3@gmail.com>
2023-07-25 09:17:35 +0530

    Commit Odd Number :

---
+++ src/Loops_Working/Display_Odd_Number.java
@@ -0,0 +1,13 @@
+package Loops_Working;
+
+public class Display_Odd_Number {
+    public static void main(String[] args) {
+        int number=100;
+        for (int i=1;i<=100;i++){
+            if (i%2==1){
+                System.out.println("Printing odd number is -----> " +i);
+            }
+        }
+    }
+
+}
