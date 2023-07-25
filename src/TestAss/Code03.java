2e8edc49088819c1e2285c9856a3133d8ef2ac2d
Rohan030723 <rohan301997@gmail.com>
2023-07-25 20:48:34 +0530

    Code03

--- src/TestAss/Code03.java
+++ src/TestAss/Code03.java
@@ -5,12 +5,18 @@ public class Code03 {
     public static void main(String[] args) {
 
         int age =18;
-        String City = "Pune";
+
 
         if(age>15){
-            System.out.println("Eligible for voting in pune city");
+            System.out.println("Eligible for voting...");
         }else {
-            System.out.println("Not eligible for voting in pune city");
+            System.out.println("Not eligible for voting..");
         }
+        String City = "Pune";
+         if(City=="Pune"){
+             System.out.println("You are eligible for voting in Pune");
+         }else{
+             System.out.println("You are not eligible for voting in Pune");
+         }
     }
 }
