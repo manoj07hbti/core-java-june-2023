a42938a3d2f0fa4902d90eb9bacdf5c0614b9bea
RamD-1998 <maruti@1998>
2023-07-25 09:37:41 +0530

    Table of five_loop

---
+++ src/Loop_working_project/Table_of_five.java
@@ -0,0 +1,13 @@
+package Loop_working_project;
+
+public class Table_of_five {
+    public static void main(String[] args) {
+
+        int number=5;
+
+        for(int i=1; i<=10; i++){
+            System.out.println(number +"x" +i+"=" +number*i);
+
+        }
+    }
+}
