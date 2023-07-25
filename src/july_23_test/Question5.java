fd82357264c8a2951308d2a56634999928a396a1
Mayank6918-svh <mayanksingh25104@gmail.com>
2023-07-25 20:54:01 +0530

    QUESTION 5

---
+++ src/july_23_test/Question5.java
@@ -0,0 +1,13 @@
+package july_23_test;
+
+public class Question5 {
+
+    public static void main(String[] args) {
+        int number=100;
+        for (int i=1;i<=100;i++){
+            if (i%2==0){
+                System.out.println("PRINT EVEN NUMBERS "+i);
+            }
+        }
+    }
+}
