6a4d259e4ebcb842c6dc9e032dcd1ebb2fce6c69
pcz <bachchoomathur3@gmail.com>
2023-07-25 20:39:13 +0530

    Commit Percentage   :

---
+++ src/July_25_2023_test/Percentage.java
@@ -0,0 +1,9 @@
+package July_25_2023_test;
+
+public class Percentage {
+    public static void main(String[] args) {
+        int a=500;
+            a=500*3/100;
+        System.out.println("Printing Percentage is : "   +a);
+    }
+}
