596b320be06d4aefa937ec3ac7042bb4dc14348d
rajoriya2023 <rajoriyaprashant1992@gmail.com>
2023-07-25 21:43:48 +0530

    25 july test odd number

--- src/july_25_test/July_25_test.java
+++ src/july_25_test/July_25_test.java
@@ -4,29 +4,35 @@ public class July_25_test {
     public static void main(String[] args) {
 
 
-            double number=500;
-            double percentage=500*5/100;
-            System.out.println("the percentage value of 500 at 5% is : " + percentage);
+        double number = 500;
+        double percentage = 500 * 5 / 100;
+        System.out.println("the percentage value of 500 at 5% is : " + percentage);
 
-            int age=75;
-            String city="pune";
-            if(age>=18 && city=="pune"){
-                System.out.println("person is eligible for voting in pune");
-            }else{
-                System.out.println("person is not eligible for voting in pune");
-            }
+        int age = 75;
+        String city = "pune";
+        if (age >= 18 && city == "pune") {
+            System.out.println("person is eligible for voting in pune");
+        } else {
+            System.out.println("person is not eligible for voting in pune");
+        }
 
-            int a=100;
-            for(int i=1; i<=100; i++){
-                if(i%2==0)
-                System.out.println("printing the even number id : " +i);
+        int a = 100;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("printing the even number id : " + i);
             }
 
         }
-
-
-
+        number=300;
+        for(int i=1; i<=300; i++){
+            if(i%3==1){
+                System.out.println("printing the value of odd number 300 is : " + i);
+            }
         }
+
+
+    }
+}
 
 
 
---
+++ src/july_25_test/Odd_numbber.java
@@ -0,0 +1,12 @@
+package july_25_test;
+
+public class Odd_numbber {
+    public static void main(String[] args) {
+        int number = 300;
+        for (int i = 1; i <= 300; i++) {
+            if (i % 3 == 1) {
+                System.out.println("printing the value of odd number : " + i);
+            }
+        }
+    }
+}
