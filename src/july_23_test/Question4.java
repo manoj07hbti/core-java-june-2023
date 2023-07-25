dd064e581d9e3055501e0bcfc4b49bf16e3646d1
Mayank6918-svh <mayanksingh25104@gmail.com>
2023-07-25 21:42:08 +0530

    QUESTION 4

---
+++ src/july_23_test/Question4.java
@@ -0,0 +1,9 @@
+package july_23_test;
+
+public class Question4 {
+    public static void main(String[] args) {
+        double num=300;
+        double percentage=300*3/100;
+        System.out.println("% of 300: "+percentage);
+    }
+}
