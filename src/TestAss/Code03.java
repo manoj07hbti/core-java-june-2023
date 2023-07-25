8ab2175777420f1b6f266a3b7a3bacc8d262cd7b
Rohan030723 <rohan301997@gmail.com>
2023-07-25 20:38:26 +0530

    Code03

---
+++ src/TestAss/Code03.java
@@ -0,0 +1,16 @@
+package TestAss;
+
+public class Code03 {
+
+    public static void main(String[] args) {
+
+        int age =18;
+        String City = "Pune";
+
+        if(age>15){
+            System.out.println("Eligible for voting in pune city");
+        }else {
+            System.out.println("Not eligible for voting in pune city");
+        }
+    }
+}
