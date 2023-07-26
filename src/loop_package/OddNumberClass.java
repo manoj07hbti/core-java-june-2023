e436499c645cdd41635a104d110ffed7615b64b2
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-26 19:18:30 +0530

    my loop class

---
+++ src/loop_package/OddNumberClass.java
@@ -0,0 +1,18 @@
+package loop_package;
+
+public class OddNumberClass {
+
+    public static void main(String[] args) {
+
+        int start= 1;
+        int end = 100;
+        System.out.println("odd number between 1 to 100");
+for (int i= start; i<=end; i++){
+    if (i% 2 !=0){
+        System.out.println(i + " ");
+    }
+}
+
+    }
+
+}
---
+++ src/loop_package/Table_of_Five.java
@@ -0,0 +1,17 @@
+package loop_package;
+
+public class Table_of_Five {
+    public static void main(String[] args) {
+
+        int number= 5;
+        for(int i=1; i<=10; i++){
+            System.out.println(number +"X" +i+"="+number*i);
+
+        }
+
+
+
+
+
+    }
+}
