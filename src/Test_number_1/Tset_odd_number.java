c0be7a1a23d41a005188a99f45555b9d50f089d8
RamD-1998 <maruti@1998>
2023-07-26 09:03:53 +0530

    test odd number

---
+++ src/Test_number_1/Tset_odd_number.java
@@ -0,0 +1,13 @@
+package Test_number_1;
+
+public class Tset_odd_number {
+    public static void main(String[] args) {
+
+        int number=300;
+        for (int i=1;i<=300;i++){
+            if (i%3==1){
+                System.out.println("Printing Odd number is only : "  +i);
+            }
+        }
+    }
+}
