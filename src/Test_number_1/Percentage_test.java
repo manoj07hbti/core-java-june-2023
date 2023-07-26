fb5f730e3a83032042c891598ef59747ceb6af1b
RamD-1998 <maruti@1998>
2023-07-26 08:51:38 +0530

    percentage_test

---
+++ src/Test_number_1/Percentage_test.java
@@ -0,0 +1,13 @@
+package Test_number_1;
+
+public class Percentage_test {
+    public static void main(String[] args) {
+
+        int number=500;
+
+        int percentage= 500 * 5 / 100;
+
+        System.out.println("printing the value of 500 by 5 persentage" +"=" +percentage);
+    }
+    }
+
