7d8c76a381693460c7d6ad1b37064d552f928e6a
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-26 10:37:12 +0530

    LOOPS EVEN NUMBER ASSIGNMENT

---
+++ src/loops/EvenNumber.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        int number=100;
+        for(int i=1; i<=100; i++){
+            if(i%2==0){
+                System.out.println("Even Numbers Are :"+i);
+            }
+
+        }
+    }
+}
---
+++ src/loops/TableOfTen.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class TableOfTen {
+    public static void main(String[] args) {
+        int number = 10;
+        for(int i=1; i<=10; i++){
+            System.out.println(number+"X" +i+" = "+number*i);
+        }
+
+    }
+}
