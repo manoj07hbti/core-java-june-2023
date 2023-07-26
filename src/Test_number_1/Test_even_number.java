b63d82e062e1f31ec002604a2510860cedc74ce3
RamD-1998 <maruti@1998>
2023-07-26 08:57:27 +0530

    test even no. 1

---
+++ src/Test_number_1/Test_even_number.java
@@ -0,0 +1,14 @@
+package Test_number_1;
+
+public class Test_even_number {
+    public static void main(String[] args) {
+        int number1=100;
+        for(int i=1; i<=100; i++){
+            if (i%2==0){
+                System.out.println("Even Number1 is :"+i);
+            }
+        }
+
+
+    }
+}
