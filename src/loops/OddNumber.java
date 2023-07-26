83da7a3bc87cd55b1e6a82f9c2b22f63bb7a3f39
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-26 10:39:48 +0530

    LOOPS ODD NUMBER ASSIGNMENT

---
+++ src/loops/OddNumber.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class OddNumber {
+    public static void main(String[] args) {
+        int number=100;
+        for(int i=1;i<=100;i++){
+            if(i%2!=0){
+                System.out.println("Odd Numbers Are :"+i);
+            }
+        }
+    }
+}
