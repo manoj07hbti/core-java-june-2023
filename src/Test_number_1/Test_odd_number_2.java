3d7311ba09206d78e1217fb5935ccace7b2d1e8e
RamD-1998 <maruti@1998>
2023-07-26 09:06:12 +0530

    tset odd number 2

---
+++ src/Test_number_1/Test_odd_number_2.java
@@ -0,0 +1,13 @@
+package Test_number_1;
+
+public class Test_odd_number_2 {
+    public static void main(String[] args) {
+
+        int number=100;
+        for (int i=1;i<=100;i++){
+            if (i%2==1){
+                System.out.println("Printing odd number is -----> " +i);
+            }
+        }
+    }
+}
