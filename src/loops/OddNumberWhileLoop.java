a323e14c63901278857039b2c5c608a10b21e62d
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-27 17:18:48 +0530

    ODD NUMBER WHILE LOOP ASSIGNMENT

---
+++ src/loops/OddNumberWhileLoop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class OddNumberWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 100) {
+
+            System.out.println("Odd Numbers Are :"+i);
+            if(i%2!=0);
+            i++;
+            i+=1;
+        }
+    }
+}
