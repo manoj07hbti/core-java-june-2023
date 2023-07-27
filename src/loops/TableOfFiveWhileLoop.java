ad7f7e843a84aaa66b035f251065254af50b785d
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-27 17:21:58 +0530

    TABLE OF FIVE AND TEN WHILE LOOP ASSIGNMENT

---
+++ src/loops/TableOfFiveWhileLoop.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class TableOfFiveWhileLoop {
+    public static void main(String[] args) {
+
+        int number = 5;
+        int i = 1;
+        while( i<=10 ){
+            System.out.println(number+"X" +i+" = "+number*i);
+            i++;
+        }
+    }
+}
---
+++ src/loops/TableOfTenWhileLoop.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class TableOfTenWhileLoop {
+    public static void main(String[] args) {
+
+        int number = 10;
+        int i = 1;
+        while (i<=10){
+            System.out.println(number+"X"+i+" = "+number*i);
+            i++;
+        }
+    }
+}
