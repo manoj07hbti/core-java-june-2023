cd9913a6b0a9784dd17e716de8e0ed04dfa86871
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-27 16:14:58 +0530

    COMMIT DO WHILE LOOP

---
+++ src/loops/DoWhileLoop.java
@@ -0,0 +1,23 @@
+package loops;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+         int i=0;
+         do {
+             System.out.println("this is me :"+i);
+             i++;
+         }while (i<5);
+
+
+        /*===========================================================*/
+
+        int j=10;
+        do {
+            System.out.println("this is not me :"+j);
+            j--;
+        }while (j>5);
+
+
+    }
+}
