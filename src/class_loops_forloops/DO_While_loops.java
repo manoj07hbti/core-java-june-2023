80df5b4cf442b7e7eeaf84baf6ef599af3caa160
Uday6667 <yadavuday63@gmail.com>
2023-07-27 22:46:08 +0530

    my do while lloop assingment is completed

---
+++ src/class_loops_forloops/DO_While_loops.java
@@ -0,0 +1,39 @@
+package class_loops_forloops;
+
+public class DO_While_loops {
+    public static void main(String[] args) {
+
+        int i=0;
+do {
+    System.out.println(+i);
+
+    i++;
+} while (i<=100);
+
+
+int j =100;
+
+do {
+    System.out.println(+j);
+    j--;
+
+        } while (j>0);
+
+int l = 0;
+
+do {
+    System.out.println(l*6);
+    l++;
+
+} while (l<=10);
+
+int b=0;
+
+do {
+    System.out.println(b*8);
+    b++;
+}while (b<=10);
+
+    }
+
+}
