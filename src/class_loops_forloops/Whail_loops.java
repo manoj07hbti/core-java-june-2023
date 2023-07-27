6abb8b6c4f38a661434d05167dfb51a688d705ee
Uday6667 <yadavuday63@gmail.com>
2023-07-27 20:33:16 +0530

    my while lloop assingment is completed

---
+++ src/class_loops_forloops/Whail_loops.java
@@ -0,0 +1,35 @@
+package class_loops_forloops;
+
+public class Whail_loops {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 100) {
+
+                System.out.println(+i);
+            i++;
+        }
+int j=100;
+
+        while (j>0)
+        {
+            System.out.println(+j);
+            j--;
+        }
+
+ int k=1;
+
+        while (k<=10){
+            System.out.println(k*9);
+            k++;
+
+
+    }
+int h=0;
+
+        while (h<=10){
+
+            System.out.println(h*4);
+            h++;
+        }
+    }
+}
--- src/test_25_07_2023_java/Persentage_test.java
+++ src/test_25_07_2023_java/Persentage_test.java
@@ -3,10 +3,33 @@ package test_25_07_2023_java;
 public class Persentage_test {
 
     public static void main(String[] args) {
-        int number=500;
+        int number = 500;
 
-        int persentage= 500 * 5 / 100;
+        int persentage = 500 * 5 / 100;
 
-        System.out.println("printing the value of 500 by 5 persentage" +"=" +persentage);
+        System.out.println("printing the value of 500 by 5 persentage" + "=" + persentage);
+        double num = 500;
+
+        num = 500 * 5 / 100;
+
+        System.out.println("print persentage" + "=" + num);
+
+
+        int num1 = 250;
+        for (int i = 1; i <= 250; i++
+) {
+            if (i % 2 == 0) {
+                System.out.println("print even number" + "=" + i);
+            }
+
+            for (int j = 1; j <= 250; j++) {
+                if (j % 3 == 0) {
+                    System.out.println("print odd number" + "=" + j);
+                }
+
+
+             }
+
+        }
     }
 }
