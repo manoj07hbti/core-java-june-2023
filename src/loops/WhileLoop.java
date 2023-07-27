9352506b724b0916329c3d4f5f85a515be432b03
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-27 16:14:12 +0530

    COMMIT WHILE LOOP

---
+++ src/loops/WhileLoop.java
@@ -0,0 +1,28 @@
+package loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i <= 5) {
+
+            System.out.println("This was MIR: " + i);
+
+            i++;
+        }
+
+
+     /*======================================================================*/
+
+
+        int j = 10;
+        while (j > 0) {
+
+
+            System.out.println("This is International Space Station: " + j);
+            j--;
+        }
+
+    }
+}
