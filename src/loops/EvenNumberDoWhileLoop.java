a36d434d5f8ada17a472aab12bb32971ae61be8e
jatin12345n <jitinshakya786jks@gmail.com>
2023-07-27 17:20:36 +0530

    EVEN NUMBER DO WHILE LOOP ASSIGNMENT

---
+++ src/loops/EvenNumberDoWhileLoop.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class EvenNumberDoWhileLoop {
+    public static void main(String[] args) {
+        int i=2;
+        do {
+            System.out.println("Even Numbers Are :"+i);
+            i++;
+            i+=1;
+            if(i%2==0);
+        }while (i<=100);
+    }
+}
