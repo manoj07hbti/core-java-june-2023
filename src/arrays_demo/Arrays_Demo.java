bd14231bab7a74dcd62e9deedac63a9764eafcbd
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-29 00:37:47 +0530

    Arrays_Demo

---
+++ src/arrays_demo/Arrays_Demo.java
@@ -0,0 +1,22 @@
+package arrays_demo;
+public class Arrays_Demo {
+    public static void main(String[] args) {
+
+        int numbers[] = {234, 888, 997, 778};
+        for (int var : numbers) {
+            System.out.println("printing Array " + var);
+        }
+
+        String Place[] = {"Agra ", "Mumbai ", "Pune" , "Delhi "};
+        for (String var : Place){
+            System.out.println("Printing Array " +var);
+        }
+
+        double price [] = { 25.2, 34.7,98.4,57.7};
+        for (double var : price ) {
+            System.out.println("Printing Array " +var);
+        }
+
+    }
+
+}
