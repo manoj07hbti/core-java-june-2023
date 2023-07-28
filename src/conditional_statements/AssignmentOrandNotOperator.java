273585b47534e0cec96e79ac4d00e4c312ecdcc2
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-28 18:49:51 +0530

    AssignmentOrandNotOperator

---
+++ src/conditional_statements/AssignmentOrandNotOperator.java
@@ -0,0 +1,26 @@
+package conditional_statements;
+
+public class AssignmentOrandNotOperator {
+
+        public static void main(String[] args) {
+//Write a code which can tell any positive number is devided by 3 or not .
+            int number = 18;
+            if (number > 0) {
+                if (number / 27 == 0) {
+                    System.out.println("Number is devided by 3");
+                } else {
+                    System.out.println("Number is not devided by 3");
+                }
+            }
+            int value = 27;
+            if (value<30){
+                if (value/30 ==30){
+                    System.out.println("Value is devided by 3");
+                } else {
+                    System.out.println("Value is not Devided by 3");
+                }
+            }
+
+        }
+    }
+
