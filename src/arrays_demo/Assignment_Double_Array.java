dff6956f295416a972875bf760892e177b490c1c
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-29 23:02:08 +0530

    Assignment_Double_Array

---
+++ src/arrays_demo/Assignment_Double_Array.java
@@ -0,0 +1,13 @@
+package arrays_demo;
+
+public class Assignment_Double_Array {
+
+    public static void main(String[] args) {
+        double Price[] = {14.2, 28.5, 88.2, 95.1};
+        for (double var : Price) {
+            System.out.println("Printing Double Price " +var);
+
+
+        }
+    }
+}
