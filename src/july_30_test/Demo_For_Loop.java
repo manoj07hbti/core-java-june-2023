285e84c5e427ea786a7384a1bcc28a8af914c99d
KeertiAbhay <keertikumari900@gmail.com>
2023-07-30 20:57:47 +0530

    My_TestDemo_For_Loop

---
+++ src/july_30_test/Demo_For_Loop.java
@@ -0,0 +1,22 @@
+package july_30_test;
+
+public class Demo_For_Loop {
+    public static void main(String [] args){
+        int start = -3;
+        int end = 3;
+
+        int current = start;
+        while (current<=end){
+            System.out.println(current+" ");
+            current++;
+
+
+        }
+
+
+
+
+
+    }
+
+}
