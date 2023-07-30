514495682b3f8362765ced389e1bbe2cfcb50f7c
Dauli-Nigam <ishayanigam842933baby@gmail.com>
2023-07-30 20:47:42 +0530

    30 july test

---
+++ src/july_30test/PrimeNumber_class.java
@@ -0,0 +1,13 @@
+package july_30test;
+
+public class PrimeNumber_class {
+    public static void main(String[] args) {
+        System.out.println("printing prime number 1 between 20");
+
+        String primeNumbers="";
+        for(int i =2; i <=20; i++);
+
+
+
+    }
+}
---
+++ src/july_30test/PrintNumberRange.java
@@ -0,0 +1,20 @@
+package july_30test;
+
+public class PrintNumberRange {
+
+    public static void main(String[] args) {
+   int start = -3;
+   int end = 3;
+   int current = start;
+   while (current <= end){
+
+       System.out.println(current +" ");
+
+       current++;
+   }
+
+
+
+    }
+
+}
