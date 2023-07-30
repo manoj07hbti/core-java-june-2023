17226f91cec95d180f9f2bef902ad8b26a703333
Rohan030723 <rohan301997@gmail.com>
2023-07-31 00:30:46 +0530

    TableofFive

---
+++ src/loop_working/TableofFive.java
@@ -0,0 +1,20 @@
+package loop_working;
+
+public class TableofFive {
+
+    public static void main(String args[]){
+
+        int number = 5;
+        for(int i=1; i<=10; i++){
+
+            System.out.println(number +"X" +i+" = "+number*i);
+
+        }
+        int numbers= 10;
+        for(int i=1; i<=10; i++){
+
+            System.out.println(numbers +"X" +i+" = "+numbers*i);
+
+        }
+    }
+}
