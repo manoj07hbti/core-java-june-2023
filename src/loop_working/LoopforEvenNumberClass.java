59b7e23264b16feee275d44079b247d2ac6a1356
Rohan030723 <rohan301997@gmail.com>
2023-07-31 01:43:17 +0530

    LoopForEvenNumberClass

---
+++ src/loop_working/LoopforEvenNumberClass.java
@@ -0,0 +1,15 @@
+package loop_working;
+
+public class LoopforEvenNumberClass {
+
+    public static void main(String[] args) {
+
+   int number = 1;
+        for(int i=0; i<=100; i++){
+            if (i%2 == 0) {
+                System.out.println( "This are even number" +i);
+            }
+
+        }
+    }
+}
