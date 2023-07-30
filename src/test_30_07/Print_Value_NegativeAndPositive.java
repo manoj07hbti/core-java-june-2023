7767a6d646cd44c869d20618e193e71c14674db0
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-30 21:16:19 +0530

    Print Value Negative and Positive

---
+++ src/test_30_07/Print_Value_NegativeAndPositive.java
@@ -0,0 +1,9 @@
+package test_30_07;
+
+public class Print_Value_NegativeAndPositive {
+    public static void main(String[] args) {
+        for (int i=-3; i<=3; i++){
+            System.out.println(i);
+        }
+    }
+}
