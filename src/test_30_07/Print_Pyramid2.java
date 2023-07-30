1a0664fa478a1e318bfb601450d74c02e46f0504
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-30 21:19:43 +0530

    Print_Pyramid2

---
+++ src/test_30_07/Print_Pyramid2.java
@@ -0,0 +1,16 @@
+package test_30_07;
+
+public class Print_Pyramid2 {
+    public static void main(String[] args) {
+        int rows = 3;
+
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
