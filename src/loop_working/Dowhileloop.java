589af4c2f868fe3eef978dd6359cad7018c3cc45
Rohan030723 <rohan301997@gmail.com>
2023-08-01 00:04:35 +0530

    Do While loop even and odd

--- src/loop_working/Dowhileloop.java
+++ src/loop_working/Dowhileloop.java
@@ -4,7 +4,7 @@ public class Dowhileloop {
     public static void main(String[] args) {
         int i=1;
         do{
-            if(i%2==0) {
+            if(i%2== 0) {
                 System.out.println("This are even number" + i);
             }else{
                 System.out.println("This are odd number" + i);
