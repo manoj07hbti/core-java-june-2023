f8660fb2fce2246c3e617201c05346635a3cf33a
RamD-1998 <maruti@1998>
2023-07-31 19:54:46 +0530

    Loop For Even Class assignment

---
+++ src/Loop_working_project/LoopForEvenClass.java
@@ -0,0 +1,13 @@
+package Loop_working_project;
+
+public class LoopForEvenClass {
+    public static void main(String[] args) {
+
+        int number=1;
+        for(int i=1; i>=1; i++){
+            if (i%2 == 0) {
+                System.out.println("there are even number" +i);
+            }
+        }
+    }
+}
