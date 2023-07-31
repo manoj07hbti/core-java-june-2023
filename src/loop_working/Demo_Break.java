ef58a3d6463c4fdc02fa7cf8a36a7ed47240da6c
anoopk2021 <anoop.k.2017@gmail.com>
2023-07-31 22:21:43 +0530

    Demo_Break

---
+++ src/loop_working/Demo_Break.java
@@ -0,0 +1,14 @@
+package loop_working;
+
+public class Demo_Break {
+
+    public static void main(String[] args) {
+        int arr [] = { 1,2,3,4,5,6,7,8,9,10};
+        for (int i=1; i<=10; i++){
+            if (i==5){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
