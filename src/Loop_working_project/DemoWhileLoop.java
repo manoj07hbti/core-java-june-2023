9e5ffdbc72e984792a50953da5da2c22e064f2d2
RamD-1998 <maruti@1998>
2023-07-31 19:38:38 +0530

    While loop table of 5

---
+++ src/Loop_working_project/DemoWhileLoop.java
@@ -0,0 +1,18 @@
+package Loop_working_project;
+
+public class DemoWhileLoop {
+    public static void main(String[] args) {
+
+        int number = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + "x" + i + " = " + number * i);
+
+        }
+        int numbers = 10;
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(numbers + "X" + i + " = " + numbers * i);
+
+        }
+    }
+}
