94862bfe2e3d8a98a7eaf8dd5707f0ce808ce99a
KeertiAbhay <keertikumari900@gmail.com>
2023-07-31 22:48:47 +0530

    Assignment_Demo_Array

--- src/array_demo_day1/Demo_array.java
+++ src/array_demo_day1/Demo_array.java
@@ -16,48 +16,52 @@ public class Demo_array {
         System.out.println("printing data in one index   :" + names[2]);
 
         for (int i = 1; i <= names.length; i++) {
-            System.out.println("Printing data in on index  :"+names[1]);
+            System.out.println("Printing data in on index  :" + names[1]);
 
         }
+            for (String var : names) {
+                System.out.println("Printing array using advanced loop  :" + var);
+            }
 
-        for (String var: names){
-            System.out.println("Printing array using advanced loop  :"+var);
+
+            double price1 = 78.96;
+            double price2 = 89.56;
+            double price3 = 45.67;
+            double price4 = 34.89;
+            double price5 = 45.67;
+
+            double prices[] = {78.96, 89.56, 45.67, 34.89, 45.67};
+            System.out.println("Printing data in one index  " + prices[1]);
+            System.out.println("Printing data in one index  " + prices[2]);
+
+
+            for (double i = 1; i <= prices.length; i++) {
+                System.out.println("Printing data in index  :" + prices[1]);
+
+            }
+
+            for (double var : prices) {
+
+                System.out.println("Printing array using advanced loop  :" + var);
+            }
+
+
+            int i = 0;
+            do {
+                System.out.println("Printing array using do while loop :" + prices[i]);
+                i++;
+            } while (i <= prices.length);
+
+
+            int i1 = 0;
+            while (i <= prices.length) {
+
+                System.out.println("Printing array using while loop :" + prices + i);
+                i++;
+
+            }
+
         }
-
-
-
-
-
-        double price1 = 78.96;
-        double price2 = 89.56;
-        double price3 = 45.67;
-        double price4 = 34.89;
-        double price5 = 45.67;
-
-        double prices[]={78.96,89.56,45.67,34.89,45.67};
-        System.out.println("Printing data in one index  "+prices[1]);
-        System.out.println("Printing data in one index  "+prices[2]);
-
-
-
-        for (double i = 1; i <= prices.length; i++){
-            System.out.println("Printing data in index  :"+prices[1]);
-
-        }
-
-        for (double var : prices){
-
-            System.out.println("Printing array using advanced loop  :"+var);
-        }
-
-
-        int i = 0;
-        do {
-            System.out.println(prices [i]);
-            i++;
-        }while (i<=prices.length);
 
 
     }
-
-}
