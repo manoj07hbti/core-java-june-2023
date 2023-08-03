14fe865f741d9faab69ed1fca5b57f2df8639765
Ankita <ankitachougule19@gmail.com>
2023-08-03 23:52:09 +0530

    testfact

--- src/july_25_test/FindFactTest.java
+++ src/july_25_test/FindFactTest.java
@@ -2,20 +2,14 @@ package july_25_test;
 
 public class FindFactTest {
     public static void main(String[] args) {
-        int num = 4;
-      //  int res=1;
-//        if(num>0)
-//
-//        {
-//           int  res=num*(num-1) ;
-//            System.out.println("factorial is:" +res);
-//        }
-//    }
-        for (int i=4; i>0; i--)
+        int num = 5;
+  int fact=1;
+        for (int i=num; i>0; i--)
         {
+            fact=fact*i;
 
-            System.out.println("Fact:\t" +i);
         }
+        System.out.print(+fact);
 
     }
 }
