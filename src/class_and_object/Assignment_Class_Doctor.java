3087408e7821b8bf373b91ca1b64a73b8364b44a
anoopk2021 <anoop.k.2017@gmail.com>
2023-08-04 22:25:32 +0530

    Assignment_Doctor

--- src/class_and_object/Assignment_Class_Doctor.java
+++ src/class_and_object/Assignment_Class_Doctor.java
@@ -1,5 +1,5 @@
 package class_and_object;
-public class Assignment_Doctor {
+public class Assignment_Class_Doctor {
 
 
    // Doctor class and 5 Object
@@ -14,7 +14,7 @@ public class Assignment_Doctor {
     String timing = "10 AM to 5 PM";
     public static void main(String[] args) {
 
-    Assignment_Doctor doctor1 = new Assignment_Doctor();
+    Assignment_Class_Doctor doctor1 = new Assignment_Class_Doctor();
         System.out.println("Doctor Name : " + doctor1.name + ", Specialist in : " + doctor1.specialist +
                 ", Location : " + doctor1.location + ", Distance " + doctor1.distance + " and Timing : " + doctor1.timing);
 
