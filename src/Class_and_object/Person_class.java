98fdf14ef08565b37523bc1f0a8991a0ffa4d158
RamD-1998 <maruti@1998>
2023-08-07 17:14:33 +0530

    object programming2

---
+++ src/Class_and_object/Person_class.java
@@ -0,0 +1,40 @@
+package Class_and_object;
+
+public class Person_class {
+
+    String person_name="ABC";
+
+    String profession="doctor";
+
+    String city="pune";
+
+    int height=6;
+
+    double salary=5000;
+
+    public static void main(String[] args) {
+
+        Person_class person1=new Person_class();
+
+        System.out.println("printing person_name:-"+person1.person_name+" profession:-"+person1.profession+" city:-"+person1.city+" height:-"+person1.height+" salary:-"+person1.salary);
+    }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
