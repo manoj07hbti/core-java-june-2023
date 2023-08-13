59967e116a0f6daf783c287e4fae38e668837c95
jatin12345n <jitinshakya786jks@gmail.com>
2023-08-13 20:13:14 +0530

    TEST 13 AUGUST

---
+++ src/august_13_test/Class1.java
@@ -0,0 +1,20 @@
+package august_13_test;
+
+public class Class1 {
+    public static void main(String[] args) {
+
+        int number = 190;
+        for (int i = 7; i <= 190; i++) {
+
+            if (i % 7 == 0 && i % 2 == 0) {
+                System.out.println("Even Numbers are :" + i);
+            }
+        }
+    }
+}
+
+
+
+
+
+
