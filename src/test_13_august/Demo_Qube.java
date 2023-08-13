e1709de25e1b0e29ddf70a04e58e2f2969054cd7
KeertiAbhay <keertikumari900@gmail.com>
2023-08-13 20:14:20 +0530

    Todays_Test_QubeNumber

---
+++ src/test_13_august/Demo_Qube.java
@@ -0,0 +1,47 @@
+package test_13_august;
+
+public class Demo_Qube {
+    public static void main(String args []){
+
+        // Write a code to print of number starting from 1 to 10 :
+        int qubeNumber = 1;
+        int result = 1*1*1;
+        System.out.println(" Qube number is:"+result);
+
+        int qubeNumber1 = 2;
+        int result1 = 2*2*2;
+        System.out.println("Qube number is:"+result1);
+
+        int qubeNumber2 = 3;
+        int result2 = 3*3*3;
+        System.out.println(" Qube number is:"+result2);
+
+        int qubeNumber3 = 4;
+        int result3 = 4*4*4;
+        System.out.println(" Qube Number is:"+result3);
+
+        int qubeNumber4 = 5;
+        int result4 = 5*5*5;
+        System.out.println(" Qube Number is:"+result4);
+
+        int qubeNumber5 = 6;
+        int result5 = 6*6*6;
+        System.out.println("Qube Number is: "+result5);
+
+        int qubeNumber6 = 7;
+        int result6 = 7*7*7;
+        System.out.println("Qube Number is:"+result6);
+
+        int qubeNumber7 = 8;
+        int result7 = 8*8*8;
+        System.out.println("Qube Number is:"+result7);
+
+        int qubeNumber8 = 9;
+        int result8 = 9*9*9;
+        System.out.println("Qube Number is:"+result8);
+
+        int qubeNumber9 = 10;
+        int result9 = 10*10*10;
+        System.out.println("Qube Number is:"+result9);
+    }
+}
