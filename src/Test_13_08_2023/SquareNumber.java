cebe918885073380fce7990ace7cdc9020fa19e9
pcz <bachchoomathur3@gmail.com>
2023-08-13 20:06:58 +0530

    Mathur (Test 14-08-2023)

---
+++ src/Test_13_08_2023/SquareNumber.java
@@ -0,0 +1,80 @@
+package Test_13_08_2023;
+
+public class SquareNumber {
+    public void Square2() {
+        int a = 2;
+        int square = a * a;
+        System.out.println("Printing Square is Two : " + square);
+    }
+
+    public void Square3() {
+        int a = 3;
+        int square = a * a;
+        System.out.println("Printing Square is Three : " + square);
+
+    }
+
+    public void Square4() {
+        int a = 4;
+        int square = a * a;
+        System.out.println("Printing Square is four : " + square);
+    }
+
+    public void Square5() {
+        int a = 5;
+        int square = a * a;
+        System.out.println("Printing Square is five : " + square);
+    }
+
+    public void Square6() {
+        int a = 6;
+        int square = a * a;
+        System.out.println("Printing Square is Six : " + square);
+    }
+
+    public void Square7() {
+        int a = 7;
+        int square = a * a;
+        System.out.println("Printing Square is Seven : " + square);
+    }
+
+    public void Square8() {
+        int a = 8;
+        int square = a * a;
+        System.out.println("Printing Square is Eight : " + square);
+    }
+
+    public void Square9() {
+        int a = 9;
+        int square = a * a;
+        System.out.println("Printing Square is nine : " + square);
+    }
+
+    public void Square10() {
+        int a = 10;
+        int square = a * a;
+        System.out.println("Printing Square is Ten : " + square);
+    }
+
+    public static void main(String[] args) {
+        SquareNumber obj = new SquareNumber();
+        obj.Square2();
+        SquareNumber obj1 = new SquareNumber();
+        obj.Square3();
+        SquareNumber obj2 = new SquareNumber();
+        obj.Square4();
+        SquareNumber obj3 = new SquareNumber();
+        obj.Square5();
+        SquareNumber obj4 = new SquareNumber();
+        obj.Square6();
+        SquareNumber obj5 = new SquareNumber();
+        obj.Square7();
+        SquareNumber obj6 = new SquareNumber();
+        obj.Square8();
+        SquareNumber obj7 = new SquareNumber();
+        obj.Square9();
+        SquareNumber obj8 = new SquareNumber();
+        obj.Square10();
+
+    }
+}
