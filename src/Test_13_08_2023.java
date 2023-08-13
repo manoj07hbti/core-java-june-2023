9965d2e1912b5e39f2328c63573a6a1bd871f710
Uday6667 <yadavuday63@gmail.com>
2023-08-13 21:33:48 +0530

    my test 13-08-2023

---
+++ src/Test_13_08_2023.java
@@ -0,0 +1,17 @@
+public class Test_13_08_2023 {
+        public static void main(String[] args) {
+
+            int number = 160;
+            for (int i = 8; i <= 160; i++) {
+
+                if (i % 11 == 0 && i % 2 == 0) {
+                    System.out.println("Even Numbers are :" + i);
+                }
+            }
+        }
+    }
+
+
+
+
+
