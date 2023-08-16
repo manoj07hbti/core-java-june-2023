120029c4b248333cd5fc9bfaf3e600caa952f331
Ankita <ankitachougule19@gmail.com>
2023-08-16 23:12:17 +0530

    TEST PRIME NUMBER

--- src/august_13_test3/FindPrimeNo.java
+++ src/august_13_test3/FindPrimeNo.java
@@ -1,11 +1,14 @@
 package august_13_test3;
+
 //*TODO WRITE A CODE TOO CHECK PRIME NUMBER 10 TO 20
 public class FindPrimeNo {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
 
-        for (int no = 10; no <=20; no++) {
-int temp=0;
+        for (int no = 10; no <=20; no++)
+        {
+            int temp=0;
             for (int i = 2; i <= no-1; i++) {
                 while (no % i == 0) {
                     temp = temp + 1;
