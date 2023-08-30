6109ef704b82f246aa6ba24f97251c4713dca920
virendra-ravat <viruravat005@gmail.com>
2023-08-30 21:04:35 +0530

    Table Use For Loops

---
+++ src/Loops/TableOf_4_UsingForLoop.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class TableOf_4_UsingForLoop {
+
+    public static void main(String[] args) {
+        int number = 4;
+        for (int i=1;i<=10;i++){
+            System.out.println("4 x "+ i + " = " + number * i);
+        }
+    }
+}
