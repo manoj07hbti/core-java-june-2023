1f50edcb226f9429b1f1836bbcf271b776ce32d3
virendra-ravat <viruravat005@gmail.com>
2023-08-30 21:03:21 +0530

    While loops

---
+++ src/Loops/While_loops.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class While_loops {
+    public static void main(String[] args) {
+
+    int i=1;
+    while (i<=15){
+        System.out.println("Printing  while loops..... "+i);
+        i++;
+    }
+    int j=10;
+    while (j>0){
+        System.out.println("Printing Decrement while loops....... "+j);
+        j--;
+    }
+}
+ }
