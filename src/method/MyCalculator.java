e3a7fa4e09e3555edee1d036ce70b57552d7c79d
KeertiAbhay <keertikumari900@gmail.com>
2023-09-06 20:54:59 +0530

    MyCalculatorApp

--- src/method/MyCalculator.java
+++ src/method/MyCalculator.java
@@ -1,71 +1,89 @@
 package method;
 
+
 import java.util.Scanner;
 
 public class MyCalculator {
 
-    public int add(int a,int b){
-        return a+b;
+    public int add(int c, int d) {
+        return c + d;
 
     }
-    public int sub(int a,int b){
-        return a-b;
+
+    public int sub(int c, int d) {
+        return c - d;
 
     }
-    public  int multiple(int a,int b){
-        return a*b;
+
+    public int multiple(int c, int d) {
+        return c * d;
 
     }
-    public  int divide(int a,int b){
-        return a/b;
+
+    public int divide(int c, int d) {
+        return c / d;
     }
-    public int cube(int number){
-        int cube= number*number*number;
+
+    public int cube(int number) {
+        int cube = number * number * number;
         return cube;
     }
-    public int fact(int number){
+
+    public int fact(int number) {
         int fact = 1;
-        for (int i = 1; i<=number; i++){
-            int fact1 = fact*i;
+        for (int i = 1; i <= number; i++) {
+            int fact1 = fact * i;
         }
         return fact;
 
     }
-    public double decimal(double percentage,int number){
-        double parc =number*10/100;
+
+    public double decimal(double per, int number) {
+        double parc = number * 10 / 100;
         return parc;
     }
-    public static void main(String [] args ){
+
+    public static void main(String[] args) {
         String flag = "Y";
-        while (flag.equals("Y"))
-            System.out.println("Please Press y");
-        Scanner scanner=new Scanner(System.in);
-        flag = scanner.nextLine();
+        MyCalculator Obj = new MyCalculator();
+        while (flag.equals("Y")){
+            Scanner scanner = new Scanner(System.in);
 
+            System.out.println("Please enter your choice: 1add. 2sub. 3multiple. 4divide. 5cube. 6fact. 7per:");
+            int choice = scanner.nextInt();
+            System.out.println("Please enter your value of c:");
+            int c = scanner.nextInt();
+            System.out.println("Please enter your value of d:");
+            int d = scanner.nextInt();
 
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.println("Please enter the value of a:");
-        int a = scanner.nextInt();
-        System.out.println("Please enter the value of b:");
-        int b = scanner.nextInt();
-        System.out.println("Please enter your choice 1add");
+            if (choice == 1) {
+                int result = Obj.add(c,d);
+                System.out.println("Print addition of given number : " + result);
+            } else if (choice == 2) {
+                int result = Obj.sub(c,d);
+                System.out.println("Print subtraction: " + result);
 
-        MyCalculator obj = new MyCalculator();
-        int choice = scanner.nextInt();
-        if (choice == 1){
-            int result =obj.add(a,b);
-            System.out.println("Print addition: "+result);
-        }else if (choice == 2){
-            int result = obj.sub(a,b);
-            System.out.println("Print subtraction: "+result);
+            } else if (choice == 3) {
+                int result = Obj.multiple(c,d);
+                System.out.println("Print multiple: " + result);
+            } else if (choice == 4) {
+                int resul = Obj.divide(c,d);
+                System.out.println("Print divide: " + resul);
+            }else  if (choice == 5){
+                int result = Obj.cube(5);
+                System.out.println("cube of given number is:"+result);
+            }else if (choice == 6){
+                int result = Obj.fact(5);
+                System.out.println("Factorial is given number of:"+result);
+            }else if (choice == 7){
+                double result = Obj.decimal(6,600);
+                System.out.println("Print Percentage:"+result);
 
-        }else if (choice == 3){
-            int result = obj.multiple(a,b);
-            System.out.println("Print multiple: "+result);
-        }else if (choice ==4 ){
-            int resul = obj.divide(a,b);
-            System.out.println("Print divide: "+resul);
+            }
+            System.out.println("Press Y to continue.....any other kee to exit:");
+            Scanner scanner1 = new Scanner(System.in);
+            flag = scanner1.nextLine();
         }
-    }
 
+    }
 }
