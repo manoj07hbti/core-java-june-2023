8c31f31e742ea3dd01f139a3f2a96a27e7d99472
RamD-1998 <maruti@1998>
2023-09-10 10:26:16 +0530

    hashset courses

---
+++ src/Calculator/Calci_app.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+public class Calci_app {
+
+public int addition(int a,int b){
+    return a+b;
+}
+public int sub(int a,int b){
+    return a-b;
+}
+public int multiply(int a,int b){
+    return a*b;
+}
+}
