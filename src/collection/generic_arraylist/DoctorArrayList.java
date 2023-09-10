76b50b04d726185956e1b99075d039f169e7bff6
jatin12345n <jitinshakya786jks@gmail.com>
2023-09-10 11:47:53 +0530

    collection re-commit

--- src/collection/generic_arraylist/DoctorArrayList.java
+++ src/collection/generic_arraylist/DoctorArrayList.java
@@ -1,24 +1,23 @@
-package collection;
+package collection.generic_arraylist;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import collection.model.Doctor;
-import collection.model.Employee;
-import collection.model.Student;
 
 public class DoctorArrayList {
 
 
     public ArrayList<Doctor> doctorDataWithUserInput() {
+
         ArrayList<Doctor> doctorData = new ArrayList<>();
+
+
         return doctorData;
     }
 
     public static void main(String[] args) {
         String flag = "y";
-
-        DoctorArrayList obj = new DoctorArrayList();
 
         while (flag.equals("y")) {
             Scanner scanner = new Scanner(System.in);
@@ -26,25 +25,33 @@ public class DoctorArrayList {
             String name = scanner.nextLine();
             System.out.println("Please Enter Fees :");
             int fess = scanner.nextInt();
-            Scanner scanner1 = new Scanner(System.in);
 
+
+            Scanner scanner1 = new Scanner(System.in);
             System.out.println("Please Enter Doctor's Dept :");
             String specialist = scanner1.nextLine();
 
-            new Doctor(name, fess, specialist);
 
-            System.out.println("Doctor's Data Created As :");
-            System.out.println("Name is : " + name +" | " +" Fess is : " + fess + " | " +" Department is : " + specialist);
+
+            System.out.println(" ");
+
+
 
 
             System.out.println("Press Y to continue........and any key to Exit");
-            Scanner scanner2 = new Scanner(System.in);
+            Scanner scanner3 = new Scanner(System.in);
+            flag = scanner3.nextLine();
 
-            flag = scanner2.nextLine();
+            Doctor doctor=new Doctor(name,fess,specialist);
+            System.out.println("Doctor's Data Created As :");
+            System.out.println("Name is : " + doctor.getDoctorName() + " | " + " Fess is : " + doctor.getFees() + " | " + " Department is : " + doctor.getSpecialist());
+
+            DoctorArrayList obj = new DoctorArrayList();
+            obj.doctorDataWithUserInput();
 
         }
 
-
     }
+
 
 }
